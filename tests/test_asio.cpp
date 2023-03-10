f4a95dee4fbacf0914f013cbf1a1d95f42376d5f
João Oliveira <44142506+Joaopmoliveira@users.noreply.github.com>
2023-03-10 00:25:01 -1100

    cleaning up before first build

--- tests/test_asio.cpp
+++ tests/test_asio.cpp
@@ -44,11 +44,11 @@ read messages from sockets and pass them along to our client/server API.
 The socket is an abstraction of
 the underlying socket of asio.
 */
-struct Socket {
+class Socket {
 	asio::ip::tcp::socket _socket;
 	asio::io_context& _cxt;
 	std::list<std::shared_ptr<curan::utils::memory_buffer>> to_send;
-
+public:
 	Socket(asio::io_context& io_context,
 		const asio::ip::tcp::resolver::results_type& endpoints) : _cxt(io_context),
 		_socket(io_context) {
@@ -65,6 +65,10 @@ struct Socket {
 
 	~Socket() {
 		_socket.close();
+	}
+
+	inline asio::ip::tcp::socket& get_underlying_socket() {
+		return _socket;
 	}
 
 	void handle_connect(std::error_code ec, asio::ip::tcp::endpoint e) {
@@ -86,7 +90,7 @@ struct Socket {
 
 	void do_write()
 	{
-		asio::async_write(_socket,
+		asio::async_write(get_underlying_socket(),
 			asio::buffer(to_send.front()->begin()->data(), to_send.front()->begin()->size()),
 			[this](std::error_code ec, std::size_t /*length*/){
 				if (!ec){
@@ -95,15 +99,18 @@ struct Socket {
 						do_write();
 				}
 				else
-					_socket.close();
+					get_underlying_socket().close();
 			});
 	}
 
 	void close(){
-		asio::post(_cxt, [this]() { _socket.close(); });
+		asio::post(_cxt, [this]() { get_underlying_socket().close(); });
 	}
 };
 
+/*
+Interface for the openigtlink protocol.
+*/
 using callable_openigtlink = std::function<void(size_t, std::error_code, igtl::MessageBase::Pointer)>;
 
 /*
@@ -159,6 +166,8 @@ public:
 	struct Info {
 		asio::io_context& io_context;
 		callable connection_type;
+
+		Info(asio::io_context& io_context, callable connection_type) :io_context{ io_context }, connection_type{ connection_type } {}
 	};
 
 	Client(Info& info) : _cxt{ info.io_context }, socket{ _cxt }, connection_type{info.connection_type} {
@@ -201,6 +210,9 @@ class Server {
 public:
 	struct Info {
 		asio::io_context& io_context;
+		callable connection_type;
+
+		Info(asio::io_context& io_context, callable connection_type) :io_context{ io_context }, connection_type{ connection_type } {}
 	};
 
 	Server(Info& info) : _cxt{ info.io_context }, socket{ _cxt } {
@@ -215,53 +227,60 @@ public:
 		callables.push_back(val);
 		return cancel;
 	}
+
+	void write(std::shared_ptr<curan::utils::memory_buffer> buffer) {
+		socket.post(std::move(buffer));
+	}
 };
 
 namespace protocols {
 	namespace igtlink {
+		namespace implementation {
+			struct carry_on_luggage {
+				igtl::MessageBase::Pointer message_to_receive;
+				igtl::MessageBase::Pointer header_to_receive;
+				Client* owner = nullptr;
 
-		struct carry_on_luggage {
-			igtl::MessageBase::Pointer message_to_receive;
-			igtl::MessageBase::Pointer header_to_receive;
-			Client* owner = nullptr;
+				carry_on_luggage(Client* supplied_owner) {
+					header_to_receive->AllocatePack();
+					owner = supplied_owner;
+				}
+			};
 
-			carry_on_luggage(Client* supplied_owner) {
-				header_to_receive->AllocatePack();
-				owner = supplied_owner;
+			void read_header(carry_on_luggage val) {
+				asio::async_read(val.owner->get_socket().get_underlying_socket(),
+					asio::buffer(val.header_to_receive->GetBufferPointer(), val.header_to_receive->GetBufferSize()),
+					[val](std::error_code ec, std::size_t len)
+					{
+						if (!ec && val.header_to_receive->Unpack())
+							read_body(std::move(val));
+						else
+							val.owner->get_socket().get_underlying_socket().close();
+					});
 			}
-		};
+
+			void read_body(carry_on_luggage val) {
+				val.message_to_receive->SetMessageHeader(val.header_to_receive);
+				val.message_to_receive->AllocatePack();
+				asio::async_read(val.owner->get_socket().get_underlying_socket(),
+					asio::buffer(val.message_to_receive->GetBufferPointer(), val.message_to_receive->GetBufferSize()),
+					[val](std::error_code ec, std::size_t len)
+					{
+						if (!ec && val.header_to_receive->Unpack())
+							read_header(std::move(val));
+						else
+							val.owner->get_socket().get_underlying_socket().close();
+					});
+			}
+		}
 
 		void start(Client* client_pointer) {
-			carry_on_luggage val{ client_pointer };
+			implementation::carry_on_luggage val{ client_pointer };
 			val.owner = client_pointer;
-			read_header(std::move(val));
+			implementation::read_header(std::move(val));
 		}
 
-		void read_header(carry_on_luggage val) {
-			asio::async_read(val.owner->get_socket()._socket,
-				asio::buffer(val.header_to_receive->GetBufferPointer(), val.header_to_receive->GetBufferSize()),
-				[val](std::error_code ec, std::size_t len)
-				{
-					if (!ec && val.header_to_receive->Unpack())
-						read_body(std::move(val));
-					else
-						val.owner->get_socket()._socket.close();
-				});
-		}
 
-		void read_body(carry_on_luggage val) {
-			val.message_to_receive->SetMessageHeader(val.header_to_receive);
-			val.message_to_receive->AllocatePack();
-			asio::async_read(val.owner->get_socket()._socket,
-				asio::buffer(val.message_to_receive->GetBufferPointer(), val.message_to_receive->GetBufferSize()),
-				[vale = std::move(val)](std::error_code ec, std::size_t len)
-				{
-					if (!ec && vale.header_to_receive->Unpack())
-						read_body(std::move(vale));
-					else
-						vale.owner->get_socket()._socket.close();
-				});
-		}
 	};
 }
 
@@ -270,20 +289,27 @@ Launch a server thread which waits for a
 connection and talks through the openIGTlink
 protocol
 */
-void foo() {
-
+void foo(asio::io_context& cxt) {
+	callable_openigtlink igtlink_interface;
+	Server::Info construction{ cxt,igtlink_interface };
+	Server server{ construction };
+	for (size_t message_counter = 0; message_counter < 20; ++message_counter) {
+		
+	}
 }
 
 /*
 Launch a client which connects itself to 
 the server running inside foo.
 */
-void bar() {
-
+void bar(asio::io_context& cxt) {
+	callable_openigtlink igtlink_interface;
+	Client::Info construction{ cxt,igtlink_interface };
+	Client client{ construction };
 }
 
 int main() {
-
+	asio::io_context io_context;
 
 	return 0;
 }
