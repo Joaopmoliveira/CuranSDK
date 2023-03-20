44cdfb1a985702e4f35c79ce2fe70cb72e430c58
João Oliveira <44142506+Joaopmoliveira@users.noreply.github.com>
2023-03-20 10:51:58 +0000

    Proposed change to single container

--- libraries/userinterface/include/userinterface/widgets/Container.h
+++ libraries/userinterface/include/userinterface/widgets/Container.h
@@ -8,11 +8,37 @@
 
 namespace curan {
 	namespace ui {
-		class Container : public Drawable {
-		public:
+		class Container : public Drawable , Lockable<Container>{
+
 			SkPaint paint_layout;
 			std::vector<std::shared_ptr<Drawable>> contained_layouts;
 			std::vector<SkRect> rectangles_of_contained_layouts;
+
+		public:
+			struct InfoLinearContainer {
+				Arrangement arrangement;
+				std::vector<std::shared_ptr<Drawable>> layouts;
+				std::vector<double> divisions;
+				SkPaint paint_layout;
+			};
+
+			struct InfoVariableContainer {
+				std::vector<std::shared_ptr<Drawable>> layouts;
+				std::vector<SkRect> rectangles_of_contained_layouts;
+				SkPaint paint_layout;
+			};
+
+			Container(InfoLinearContainer& info);
+			Container(InfoVariableContainer& info);
+
+			std::shared_ptr<Container> make(InfoLinearContainer& info);
+			std::shared_ptr<Container> make(InfoVariableContainer& info);
+
+			drawablefunction draw() override;
+			callablefunction call() override;
+			void framebuffer_resize() override;
+
+
 		};
 	}
 }
---
+++ libraries/userinterface/src/Container.cpp
@@ -0,0 +1,99 @@
+#include "userinterface/widgets/Container.h"
+
+namespace curan {
+	namespace ui {
+
+		Container::Container(InfoLinearContainer& info) {
+			paint_layout = info.paint_layout;
+			contained_layouts = info.layouts;
+
+			if (contained_layouts.size() == 0)
+				return;
+			SkScalar packet_width = 1.0f / contained_layouts.size();
+
+			switch (info.arrangement) {
+			case Arrangement::HORIZONTAL:
+				if ((info.divisions.size() != 0) && (info.divisions.size() - 1 == contained_layouts.size())) {
+					for (int i = 0; i < info.divisions.size() - 1; ++i) {
+						SkRect widget_rect = SkRect::MakeLTRB(info.divisions[i], 0, info.divisions[i + 1], 1);
+						rectangles_of_contained_layouts.push_back(widget_rect);
+					}
+				}
+				else {
+					for (int i = 0; i < info.divisions.size() - 1; ++i) {
+						SkRect widget_rect = SkRect::MakeLTRB(0, info.divisions[i], 1, info.divisions[i + 1]);
+						rectangles_of_contained_layouts.push_back(widget_rect);
+					}
+				}
+				break;
+			case Arrangement::VERTICAL:
+				if ((info.divisions.size() != 0) && (info.divisions.size() - 1 == contained_layouts.size())) {
+					for (SkScalar left_position = 0.0; left_position < 1.0; left_position += packet_width) {
+						SkRect widget_rect = SkRect::MakeLTRB(left_position, 0, left_position + packet_width, 1);
+						rectangles_of_contained_layouts.push_back(widget_rect);
+					}
+				}
+				else {
+					for (SkScalar top_position = 0.0; top_position < 1.0; top_position += packet_width) {
+						SkRect widget_rect = SkRect::MakeLTRB(0, top_position, 1, top_position + packet_width);
+						rectangles_of_contained_layouts.push_back(widget_rect);
+					}
+				}
+				break;
+			default:
+
+				break;
+			}
+		}
+
+		Container::Container(InfoVariableContainer& info) {
+			paint_layout = info.paint_layout;
+			contained_layouts = info.layouts;
+			rectangles_of_contained_layouts = info.rectangles_of_contained_layouts;
+		}
+
+		std::shared_ptr<Container> Container::make(InfoLinearContainer& info) {
+			return std::make_shared<Container>(info);
+		}
+
+		std::shared_ptr<Container> Container::make(InfoVariableContainer& info) {
+			return std::make_shared<Container>(info);
+		}
+
+		drawablefunction Container::draw() {
+			auto lamb = [this](SkCanvas* canvas) {
+				std::lock_guard<std::mutex> g{ get_mutex() };
+				SkRect rectangle = get_position();
+				canvas->drawRect(rectangle, paint_layout);
+			};
+			return lamb;
+		}
+
+		callablefunction Container::call() {
+			auto lamb = [this](Signal canvas) {
+				return false;
+			};
+			return lamb;
+		}
+
+		void Container::framebuffer_resize() {
+			std::lock_guard<std::mutex> g{ get_mutex() };
+			auto iter_rect = rectangles_of_contained_layouts.begin();
+			auto iter_drawables = contained_layouts.begin();
+			auto my_position = get_position();
+			double x_accumulator = 0.0;
+			double y_accumulator = 0.0;
+			for (; iter_rect != rectangles_of_contained_layouts.end() && iter_drawables != contained_layouts.end(); ++iter_rect, ++iter_drawables) {
+				SkRect rect = *iter_rect;
+				SkRect temp = rect.MakeXYWH(my_position.x() + x_accumulator,
+					my_position.y() + y_accumulator,
+					my_position.width() * rect.width(),
+					my_position.height() * rect.height());
+				x_accumulator += rect.x() * my_position.width();
+				y_accumulator += rect.y() * my_position.height();
+				(*iter_drawables)->set_position(temp);
+				(*iter_drawables)->framebuffer_resize();
+			}
+		}
+	}
+}
