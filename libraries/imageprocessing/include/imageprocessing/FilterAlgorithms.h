8825bdce57c42f1002535ab89d1bbe73030c0012
João Oliveira <44142506+Joaopmoliveira@users.noreply.github.com>
2023-03-23 17:58:38 +0000

    Filtering scheme potential arquitecture. This will need to be properlly investigated. But we will see

--- libraries/imageprocessing/include/imageprocessing/FilterAlgorithms.h
+++ libraries/imageprocessing/include/imageprocessing/FilterAlgorithms.h
@@ -1,36 +1,50 @@
-#ifndef CURAN_SEGMENTATION_HEADER_FILE_
-#define CURAN_SEGMENTATION_HEADER_FILE_
+#ifndef CURAN_FILTER_ALGORITHMS_HEADER_FILE_
+#define CURAN_FILTER_ALGORITHMS_HEADER_FILE_
 
 #include "ImageProcessingDefinitions.h"
 #include "itkBinaryThresholdImageFilter.h"
 #include <vector>
 #include "utils/Lockable.h"
+#include <memory>
 
 namespace curan {
     namespace image {
         namespace filtering {
 
-            class Filter {
-
-                virtual bool update() = 0;
-                virtual void get_output() = 0;
-                virtual void set_input() = 0;
+            class Implementation {
+            public:
+                virtual InternalImageType::Pointer update() = 0;
+                virtual InternalImageType* get_output() = 0;
+                virtual void set_input(const InternalImageType*) = 0;
             };
 
-            class CompositFilter {
-                std::vector<std::shared_ptr<Filter>> filters;
+            class Filter : utils::Lockable<Filter> {
 
+                InternalImageType::Pointer input;
+                InternalImageType::Pointer output;
+
+                std::list<std::shared_ptr<Implementation>> filters;
+
+            public:
+
+                void operator<< (std::shared_ptr<Implementation>);
+            
+                InternalImageType::Pointer get_input();
+                InternalImageType::Pointer get_output();
             };
             
-            class ThreholdFilter : public Filter , utils::Lockable<ThreholdFilter> {
+            class ThreholdFilter : public Implementation, utils::Lockable<ThreholdFilter> {
             public:
 
                 struct Info {
                     char_pixel_type lower_bound = 0;
                     char_pixel_type upper_bound = 255;
+                    char_pixel_type outside_value = 0;
                 };
 
             private:
+
+                Filter* owner = nullptr;
 
                 using FilterType = itk::ThresholdImageFilter<InternalImageType>;
                 FilterType::Pointer filter;
@@ -39,25 +53,30 @@ namespace curan {
 
                 ThreholdFilter(Info& info);
 
+                friend Filter;
+
             public:
 
                 std::shared_ptr<ThreholdFilter> make(Info& info);
 
-                void submit_image(Image input);
-
                 void updateinfo(Info& info);
 
-                bool update() override;
+                InternalImageType::Pointer update() override;
+                InternalImageType* get_output() override;
+                void set_input(const InternalImageType*) override;
             };
 
-            class CannyFilter : public Filter, utils::Lockable<CannyFilter> {
+            class CannyFilter : public Implementation, utils::Lockable<CannyFilter> {
             public:
 
                 struct Info {
-
+                    
                 };
 
             private:
+
+                Filter* owner = nullptr;
+
                 using real_pixel_type = double;
                 using CastToRealFilterType = itk::CastImageFilter<char_pixel_type, real_pixel_type>;
                 using CannyFilterType = itk::CannyEdgeDetectionImageFilter<real_pixel_type, real_pixel_type>;
@@ -65,26 +84,31 @@ namespace curan {
 
                 CannyFilter(Info& info);
 
+                friend Filter;
+
             public:
 
                 std::shared_ptr<CannyFilter> make(Info& info);
 
-                void submit_image(Image input);
-
                 void updateinfo(Info& info);
                
-                bool update() override;
+                InternalImageType::Pointer update() override;
+                InternalImageType* get_output() override;
+                void set_input(const InternalImageType*) override;
             };
 
-            class BinarizeFilter : public Filter, utils::Lockable<BinarizeFilter> {
+            class BinarizeFilter : public Implementation, utils::Lockable<BinarizeFilter> {
             public:
 
                 struct Info {
                     char_pixel_type lower_bound = 0;
                     char_pixel_type upper_bound = 255;
+                    Filter* owner = nullptr;
                 };
 
             private:
+
+                Filter* owner = nullptr;
 
                 char_pixel_type lower_bound = 0;
                 char_pixel_type upper_bound = 255;
@@ -93,15 +117,17 @@ namespace curan {
 
                 BinarizeFilter(Info& info);
 
+                friend Filter;
+
             public:
 
                 std::shared_ptr<BinarizeFilter> make(Info& info);
 
-                void submit_image(Image input);
-
                 void updateinfo(Info& info);
 
-                bool update() override;
+                InternalImageType::Pointer update() override;
+                InternalImageType* get_output() override;
+                void set_input(const InternalImageType*) override;
             };
 
 
--- libraries/imageprocessing/src/FilterAlgorithms.cpp
+++ libraries/imageprocessing/src/FilterAlgorithms.cpp
@@ -4,24 +4,41 @@ namespace curan {
 namespace image {
 namespace filtering {
 
+void Filter::operator<< (std::shared_ptr<Implementation>) {
+
+}
+
+InternalImageType::Pointer Filter::get_input() {
+
+}
+
+InternalImageType::Pointer Filter::get_output(){
+
+};
+
 ThreholdFilter::ThreholdFilter(Info& info) {
-	
+	filter = FilterType::New();
+	filter->SetOutsideValue(info.outside_value);
+	filter->ThresholdOutside(info.lower_bound, info.upper_bound);
 }
 
 std::shared_ptr<ThreholdFilter> ThreholdFilter::make(Info& info) {
-	return std::shared_ptr<ThreholdFilter>(new ThreholdFilter{ info });
-}
-
-void ThreholdFilter::submit_image(Image input) {
-
+	return std::shared_ptr<ThreholdFilter>(new ThreholdFilter{info});
 }
 
 void ThreholdFilter::updateinfo(Info& info) {
-
+	filter->SetOutsideValue(info.outside_value);
+	filter->ThresholdOutside(info.lower_bound, info.upper_bound);
 }
 
-bool ThreholdFilter::update() {
-	return true;
+InternalImageType::Pointer ThreholdFilter::update() {
+
+}
+InternalImageType* ThreholdFilter::get_output() {
+
+}
+void ThreholdFilter::set_input(const InternalImageType*) {
+
 }
 
 CannyFilter::CannyFilter(Info& info) {
@@ -32,36 +49,44 @@ std::shared_ptr<CannyFilter> CannyFilter::make(Info& info) {
 	return std::shared_ptr<CannyFilter>(new CannyFilter{ info });
 }
 
-void CannyFilter::submit_image(Image input) {
-
-}
-
 void CannyFilter::updateinfo(Info& info) {
 
 }
 
-bool CannyFilter::update() {
-	return true;
+InternalImageType::Pointer CannyFilter::update() {
+
+}
+
+InternalImageType* CannyFilter::get_output() {
+
+}
+
+void CannyFilter::set_input(const InternalImageType*) {
+
 }
 
 BinarizeFilter::BinarizeFilter(Info& info) {
 
 }
 
-std::shared_ptr<BinarizeFilter> BinarizeFilter::make(Info& info) {
-	return std::shared_ptr<BinarizeFilter>(new BinarizeFilter{info});
-}
-
-void BinarizeFilter::submit_image(Image input) {
-
+std::shared_ptr<BinarizeFilter>BinarizeFilter::make(Info& info) {
+	return std::shared_ptr<BinarizeFilter>(new BinarizeFilter{ info });
 }
 
 void BinarizeFilter::updateinfo(Info& info) {
 
 }
 
-bool BinarizeFilter::update() {
-	return true;
+InternalImageType::Pointer BinarizeFilter::update() {
+
+}
+
+InternalImageType* BinarizeFilter::get_output() {
+
+}
+
+void BinarizeFilter::set_input(const InternalImageType*) {
+
 }
 
 }
