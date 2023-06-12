af2bc64877505766c06a85ff9a583497bc1e5d05
João Oliveira <44142506+Joaopmoliveira@users.noreply.github.com>
2023-06-12 11:25:59 +0100

    deleted not

---
+++ Curan/libraries/rendering/src/SequencialLinks.cpp
@@ -0,0 +1,78 @@
+#include "rendering/SequencialLinks.h"
+
+namespace curan {
+namespace renderable {
+
+SequencialLinks::SequencialLinks(std::filesystem::path json_path,size_t number_of_links) : number_of_links{number_of_links} {
+    std::filesystem::path models_dir = json_path.parent_path();
+    nlohmann::json tableDH = nlohmann::json::parse(std::ifstream(json_path));
+
+    this->transform = vsg::MatrixTransform::create(vsg::translate(vsg::vec3(0.0f, 0.0f, 0.0f)));
+    this->obj_contained = vsg::Group::create();
+
+    vsg::ref_ptr<vsg::Options> options = vsg::Options::create();
+    options->add(vsgXchange::all::create());
+
+    size_t transform_index = 0;
+    assert(tableDH.size()>=number_of_links && "The number of links is larger then the number of links configured in the json file");
+    
+    this->sequencial_base_transform = vsg::MatrixTransform::create(vsg::translate(vsg::vec3(0.0, 0.0, 0.0)));
+    vsg::ref_ptr<vsg::MatrixTransform> previous_object = this->sequencial_base_transform;
+    obj_contained->addChild(this->sequencial_base_transform);
+
+    for (auto& denavit_parameter : tableDH) {
+        int is_dynamic = denavit_parameter["dynamic"];
+        std::string relative_path = denavit_parameter["path"];
+        float alpha = denavit_parameter["alpha"];
+        float x_offset = denavit_parameter["x_offset"];
+        float phi = denavit_parameter["phi"];
+        float d_offset = denavit_parameter["d_offset"];
+
+        std::filesystem::path local_temp_path = models_dir;
+        local_temp_path += "/" + relative_path;
+        std::cout << local_temp_path << std::endl;
+        vsg::ref_ptr<vsg::Node> link_mesh = vsg::read_cast<vsg::Node>(local_temp_path.string() , options);
+        vsg::ref_ptr<vsg::MatrixTransform> denavit_offset = vsg::MatrixTransform::create(vsg::translate(vsg::vec3(0.0f,0.0f,0.0f)));
+        //denavit_offset->matrix = vsg::rotate(vsg::radians(alpha), 1.0f, 0.0f, 0.0f)*vsg::translate(vsg::vec3(x_offset,0.0,0.0)) * vsg::rotate(vsg::radians(phi), 0.0f, 0.0f, 1.0f)*vsg::translate(vsg::vec3(d_offset,0.0,0.0));
+        
+        if(!link_mesh)
+            throw std::runtime_error("failed to load one of the links");
+        denavit_offset->addChild(link_mesh);
+        previous_object->addChild(denavit_offset);
+        previous_object = denavit_offset;
+
+        if(is_dynamic) {
+            assert(transform_index<number_of_links && "the number of dynamic links is larger than what was compiled");
+            vsg::ref_ptr<vsg::MatrixTransform> dynamic_angle = vsg::MatrixTransform::create(vsg::rotate(vsg::radians(0.0), 0.0, 0.0, 1.0));
+            previous_object->addChild(dynamic_angle);
+            previous_object = dynamic_angle;
+            links_matrix_transform[transform_index] = dynamic_angle;
+            ++transform_index; 
+        }
+    }
+    if(transform_index!=number_of_links){
+        std::cout << "The number of links is larger then the number of links configured in the json file\n";
+    } 
+}
+
+vsg::ref_ptr<Renderable> SequencialLinks::make(std::filesystem::path json_path,size_t number_of_links) {
+    vsg::ref_ptr<SequencialLinks> arm = SequencialLinks::create(json_path,number_of_links);
+    vsg::ref_ptr<Renderable> val = arm.cast<Renderable>();
+    return val;
+}
+
+void SequencialLinks::set(const std::vector<double>& qs) {
+    for (auto& local_matrix : links_matrix_transform) {
+        size_t currentIdx = &local_matrix - &links_matrix_transform[0];
+        q[currentIdx] = qs[currentIdx];
+        local_matrix->matrix = vsg::rotate(q[currentIdx], 0.0, 1.0, 0.0);
+    }
+}
+
+void SequencialLinks::append(vsg::ref_ptr<Renderable> link_to_join, vsg::ref_ptr<vsg::MatrixTransform> relative_transformation) {
+    relative_transformation->addChild(link_to_join->transform);
+    links_matrix_transform.back()->addChild(relative_transformation);
+}
+
+}
+}
--- Curan/tests/rendering/test_robot_display.cpp
+++ Curan/tests/rendering/test_robot_display.cpp
@@ -10,45 +10,19 @@ int main(int argc, char **argv) {
         info.api_dump = false;
         info.display = "";
         info.full_screen = false;
-        info.is_debug = true;
+        info.is_debug = false;
         info.screen_number = 0;
         info.title = "myviewer";
         curan::renderable::Window::WindowSize size{1000, 800};
         info.window_size = size;
         curan::renderable::Window window{info};
 
-        std::filesystem::path robot_path = CURAN_COPIED_RESOURCE_PATH"/models/kukamedglb/arm.json";
-        vsg::ref_ptr<curan::renderable::Renderable> robotRenderable = curan::renderable::SequencialLinks<7>::make(robot_path);
-        robotRenderable->update_transform(vsg::MatrixTransform::create(vsg::translate(vsg::dvec3(0.0, 0.0, 0.43))));
+        std::filesystem::path robot_path = CURAN_COPIED_RESOURCE_PATH"/models/testing/arm.json";
+        vsg::ref_ptr<curan::renderable::Renderable> robotRenderable = curan::renderable::SequencialLinks::make(robot_path);
+        robotRenderable->update_transform(vsg::MatrixTransform::create(vsg::translate(vsg::vec3(0.0, 0.0, 0.0))));
         window << robotRenderable;
 
-        std::atomic<bool> continue_running = true;
-        auto lambda_called = [&continue_running,robotRenderable](){
-            auto robot = robotRenderable.cast<curan::renderable::SequencialLinks<7>>();
-            std::array<double,7> robot_config = {0.0};
-            std::array<double,7> step = {0.01,0.01,0.0,0.00,0.00,0.00,0.00};
-            static_assert(step.size()==robot_config.size());
-            while(continue_running.load()){
-                size_t index = 0;
-                for(auto& angle : robot_config){
-                    angle += step[index];
-                    ++index;
-                }
-                robot->set<0>(robot_config[0]);
-                robot->set<1>(robot_config[1]);
-                robot->set<2>(robot_config[2]);
-                robot->set<3>(robot_config[3]);
-                robot->set<4>(robot_config[4]);
-                robot->set<5>(robot_config[5]);
-                robot->set<6>(robot_config[6]);
-                std::this_thread::sleep_for(std::chrono::milliseconds(16));
-            }
-        };
-        //std::thread local_robot_mover{lambda_called};
-
         window.run();
-        continue_running.store(false);
-        //local_robot_mover.join();
 
         window.transverse_identifiers(
             [](const std::unordered_map<std::string, vsg::ref_ptr<curan::renderable::Renderable>>
@@ -57,11 +31,8 @@ int main(int argc, char **argv) {
                     std::cout << "Object contained: " << p.first << '\n';
             });
 
-    } catch (const vsg::Exception &ve) {
-        for (int i = 0; i < argc; ++i)
-            std::cerr << argv[i] << " ";
-        std::cerr << "\n[Exception] - " << ve.message
-                  << " result = " << ve.result << std::endl;
+    } catch (...) {
+        std::cerr << "Exception thrown" << std::endl;
         return 1;
     }
     return 0;
