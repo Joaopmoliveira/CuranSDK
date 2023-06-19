3783117ac5fed6334194111c1083cccfc9b4804a
João Oliveira <44142506+Joaopmoliveira@users.noreply.github.com>
2023-06-19 11:40:59 +0100

    Update Volume.cpp
    Start implementing the Volume from the examples provided

---
+++ Curan/libraries/rendering/src/Volume.cpp
@@ -0,0 +1,19 @@
+#include "rendering/Volume.h"
+
+namespace curan {
+namespace renderable {
+    
+Volume::Volume(Info& info){
+
+}
+
+vsg::ref_ptr<Renderable> Volume::make(Info& info){
+
+}
+
+void Volume::update_texture(updater&& update){
+
+}
+
+}
+}
