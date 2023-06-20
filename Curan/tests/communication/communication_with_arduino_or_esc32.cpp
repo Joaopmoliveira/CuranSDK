b59de606f6a25c276fadbeb447123668c94825e3
João Oliveira <44142506+Joaopmoliveira@users.noreply.github.com>
2023-06-20 18:02:29 +0100

    Add serial communication
    This example serves as a testing ground to implement the serial communication with an external device

---
+++ Curan/tests/communication/communication_with_arduino_or_esc32.cpp
@@ -0,0 +1,41 @@
+#include <asio.hpp>
+#include <iostream>
+
+constexpr size_t maximum_length_of_message = 1000; 
+
+int main(int argc, char* argv[]){
+    if(argc<2){
+        std::cout << "To use this service please provide the port number of the serial connection \neg. windows will be COM ports, linux will use /dev/ttyS* or /dev/ttyUSB*, etc\n" ;
+    }
+    std::string serial_connection_name = std::string(argv[1]);
+    asio::io_context context;
+    asio::serial_port serial(context);
+    serial.open(serial_connection_name);
+    for (;;) {
+        // get a string from the user, sentiel is exit
+        std::string input;
+        std::cout << "Enter Message: ";
+        std::cin >> input;
+
+        if (input == "exit") break;
+
+        // write to the port
+        // asio::write guarantees that the entire buffer is written to the serial port
+        asio::write(serial, asio::buffer(input));
+
+        char data[maximum_length_of_message];
+
+        // read bytes from the serial port
+        // asio::read will read bytes until the buffer is filled
+        size_t nread = asio::read(
+            serial, asio::buffer(data, input.length())
+        );
+
+        std::string message(data, nread);
+
+        std::cout << "Recieved: ";
+        std::cout << message << std::endl;
+    }
+    serial.close();
+    return 0; 
+}
