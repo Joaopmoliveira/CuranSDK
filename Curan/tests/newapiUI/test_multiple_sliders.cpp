3784577281f082f55713552e31a772458a9e4d7d
João Oliveira <44142506+Joaopmoliveira@users.noreply.github.com>
2023-07-04 16:55:07 +0100

    Update test_multiple_sliders.cpp
    Porting to new API multiple sliders example

--- Curan/tests/newapiUI/test_multiple_sliders.cpp
+++ Curan/tests/newapiUI/test_multiple_sliders.cpp
@@ -16,53 +16,47 @@
 #include <iostream>
 
 std::shared_ptr<curan::ui::Overlay> create_option_page() {
-	Container::InfoLinearContainer infocontainer;
-	infocontainer.paint_layout = paint_square2;
-	infocontainer.arrangement = curan::ui::Arrangement::HORIZONTAL;
+	using namespace curan::ui;
 
-	Slider::Info infor{};
-	infor.click_color = SK_ColorLTGRAY;
-	infor.hover_color = SK_ColorCYAN;
-	infor.waiting_color = SK_ColorDKGRAY;
-	infor.sliderColor = SK_ColorGRAY;
-	infor.paintButton = paint_square;
-	infor.size = SkRect::MakeWH(200, 40);
-	infor.limits = { 0.0f, 300.0f };
-	std::shared_ptr<Slider> button = Slider::make(infor);
+	auto slider = Slider::make({ 0.0f, 300.0f });
+	slider->set_click_color(SK_ColorDKGRAY).set_hover_color(SK_ColorCYAN).set_waiting_color(SK_ColorGRAY).set_size(SkRect::MakeWH(200, 40));
+	auto textblob = TextBlob::make("Option 1");
+	textblob->set_text_color(SK_ColorWHITE).set_background_color(SK_ColorBLACK).set_size(SkRect::MakeWH(200, 40));
 
-	TextBlob::Info infotext;
-	infotext.button_text = "Option 1";
-	infotext.paint = paint_square;
-	infotext.paintText = paint_text;
-	infotext.size = SkRect::MakeWH(200, 40);
-	infotext.textFont = text_font;
-	std::shared_ptr<TextBlob> text = TextBlob::make(infotext);
+	auto container = Container::make(Container::ContainerType::LINEAR_CONTAINER,Container::Arrangement::HORIZONTAL);
+	*container << std::move(slider) << std::move(textblob);
+	container->set_divisions({ 0.0 , 0.5 , 1.0 });
 
-	infocontainer.layouts = {text,button};
-	std::shared_ptr<Container> container = Container::make(infocontainer);
+	auto slider1 = Slider::make({ 0.0f, 300.0f });
+	slider1->set_click_color(SK_ColorDKGRAY).set_hover_color(SK_ColorCYAN).set_waiting_color(SK_ColorGRAY).set_size(SkRect::MakeWH(200, 40));
+	auto textblob1 = TextBlob::make("Option 2");
+	textblob1->set_text_color(SK_ColorWHITE).set_background_color(SK_ColorBLACK).set_size(SkRect::MakeWH(200, 40));
 
-	infotext.button_text = "Option 2";
-	std::shared_ptr<TextBlob> text1 = TextBlob::make(infotext);
-	std::shared_ptr<Slider> button1 = Slider::make(infor);
-	infocontainer.layouts = { text1,button1 };
-	std::shared_ptr<Container> container1 = Container::make(infocontainer);
+	auto container1 = Container::make(Container::ContainerType::LINEAR_CONTAINER,Container::Arrangement::HORIZONTAL);
+	*container1 << std::move(slider1) << std::move(textblob1);
+	container1->set_divisions({ 0.0 , 0.5 , 1.0 });
 
-	infotext.button_text = "Option 3";
-	std::shared_ptr<TextBlob> text2 = TextBlob::make(infotext);
-	std::shared_ptr<Slider> button2 = Slider::make(infor);
-	infocontainer.layouts = { text2,button2 };
-	std::shared_ptr<Container> container2 = Container::make(infocontainer);
+	auto slider2 = Slider::make({ 0.0f, 300.0f });
+	slider2->set_click_color(SK_ColorDKGRAY).set_hover_color(SK_ColorCYAN).set_waiting_color(SK_ColorGRAY).set_size(SkRect::MakeWH(200, 40));
+	auto textblob2 = TextBlob::make("Option 3");
+	textblob2->set_text_color(SK_ColorWHITE).set_background_color(SK_ColorBLACK).set_size(SkRect::MakeWH(200, 40));
 
-	infotext.button_text = "Option 4";
-	std::shared_ptr<TextBlob> text3 = TextBlob::make(infotext);
-	std::shared_ptr<Slider> button3 = Slider::make(infor);
-	infocontainer.layouts = { text3,button3 };
-	std::shared_ptr<Container> container3 = Container::make(infocontainer);
+	auto container2 = Container::make(Container::ContainerType::LINEAR_CONTAINER,Container::Arrangement::HORIZONTAL);
+	*container2 << std::move(slider2) << std::move(textblob2);
+	container2->set_divisions({ 0.0 , 0.5 , 1.0 });
 
-	infocontainer.arrangement = curan::ui::Arrangement::VERTICAL;
-	infocontainer.divisions = { 0.0 , 0.25 , 0.5 , 0.75 , 1.0 };
-	infocontainer.layouts = { container,container1,container2,container3};
-	std::shared_ptr<Container> containerotions = Container::make(infocontainer);
+	auto slider3 = Slider::make({ 0.0f, 300.0f });
+	slider3->set_click_color(SK_ColorDKGRAY).set_hover_color(SK_ColorCYAN).set_waiting_color(SK_ColorGRAY).set_size(SkRect::MakeWH(200, 40));
+	auto textblob3 = TextBlob::make("Option 4");
+	textblob3->set_text_color(SK_ColorWHITE).set_background_color(SK_ColorBLACK).set_size(SkRect::MakeWH(200, 40));
+
+	auto container3 = Container::make(Container::ContainerType::LINEAR_CONTAINER,Container::Arrangement::HORIZONTAL);
+	*container3 << std::move(slider3) << std::move(textblob3);
+	container3->set_divisions({ 0.0 , 0.5 , 1.0 });
+
+	auto container4 = Container::make(Container::ContainerType::LINEAR_CONTAINER,Container::Arrangement::VERTICAL);
+	*container4 << std::move(container) << std::move(container1) << std::move(container2) << std::move(container3);
+	container4->set_divisions({ 0.0 , 0.25 , 0.5 , 0.75 , 1.0 });
 
 	Overlay::Info information;
 	information.backgroundcolor = SK_ColorTRANSPARENT;
@@ -78,7 +72,7 @@ int main() {
 		std::unique_ptr<Context> context = std::make_unique<Context>();;
 		DisplayParams param{ std::move(context),2200,1800 };
 		std::unique_ptr<Window> viewer = std::make_unique<Window>(std::move(param));
-		
+
 		Button::Info infor{ resources };
 		infor.button_text = "Connect";
 		infor.click_color = SK_ColorGRAY;
