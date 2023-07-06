4ccc0ad87c80cf19d3ee1a32454daeb189ea23d0
João Oliveira <44142506+Joaopmoliveira@users.noreply.github.com>
2023-07-06 09:10:00 +0100

    Working example of the volume reconstructor
    Example of volume reconstructor

--- Curan/tests/imageprocessing/test_volume_creation.cpp
+++ Curan/tests/imageprocessing/test_volume_creation.cpp
@@ -3,6 +3,11 @@
 #include "rendering/Window.h"
 #include "rendering/Renderable.h"
 
+constexpr long width = 50;
+constexpr long height = 50;
+constexpr double offset = 1;
+float spacing[3] = {0.05 , 0.05 , 1};
+float final_spacing [3] = {1 ,1, 1};
 using imageType = curan::image::VolumeReconstructor::output_type;
 
 void updateBaseTexture3D(vsg::floatArray3D& image, imageType::Pointer image_to_render)
@@ -33,8 +38,9 @@ void updateBaseTexture3D(vsg::floatArray3D& image, imageType::Pointer image_to_r
     IteratorType outputIt(out, out->GetRequestedRegion());
     for (outputIt.GoToBegin(); !outputIt.IsAtEnd(); ++outputIt){
         imageType::IndexType idx = outputIt.GetIndex();
-        std::cout << "value: " << outputIt.Get() << std::endl;
+        //std::cout << "value: " << outputIt.Get() << std::endl;
         image.set(idx[0], idx[1], idx[2], outputIt.Get());
+        //std::printf("%f\n",outputIt.Get());
     }
 }
 
@@ -57,105 +63,47 @@ void create_array_of_linear_images_in_x_direction(std::vector<imageType::Pointer
 	image_orientation[1][2] = 0.0;
 	image_orientation[2][2] = 1.0;
 
+    std::cout << "orientation: \n" << image_orientation << std::endl;
+
 	image_origin[0] = 0.0;
 	image_origin[1] = 0.0;
 	image_origin[2] = 0.0;
 
-    constexpr long width = 3;
-    constexpr long height = 3;
-    constexpr double x_offset = 1;
-    float spacing[3] = {1.0 , 1.0 , 1.0};
-
-    imageType::Pointer image_1 = imageType::New();
-    imageType::IndexType start_1;
-    start_1[0] = 0; // first index on X
-    start_1[1] = 0; // first index on Y
-    start_1[2] = 0; // first index on Z
-
-    imageType::SizeType size_1;
-    size_1[0] = width; // size along X
-    size_1[1] = height; // size along Y
-    size_1[2] = 1; // size along Z
-
-    imageType::RegionType region_1;
-    region_1.SetSize(size_1);
-    region_1.SetIndex(start_1);
-
-    image_1->SetRegions(region_1);
-    image_1->SetDirection(image_orientation);
-    image_1->SetOrigin(image_origin);
-    image_1->SetSpacing(spacing);
-    image_1->Allocate();
     curan::image::char_pixel_type pixel[width*height];
-    curan::image::char_pixel_type pixel_value = 125;
     for(size_t y = 0; y < height ; ++y)
-        for(size_t x = 0; x < width ; ++x){
-            pixel[x+y*height] = 255-pixel_value;
-            pixel_value-=10;
-        }
+        for(size_t x = 0; x < width ; ++x)
+            pixel[x+y*height] = (int) std::sqrt(y*y+x*x);
 
-    auto pointer = image_1->GetBufferPointer();
-    std::memcpy(pointer,pixel,sizeof(curan::image::char_pixel_type)*width*height);
+    for(int z = 0; z < width ; ++z){
+        imageType::Pointer image = imageType::New();
+        imageType::IndexType start;
+        start[0] = 0; // first index on X
+        start[1] = 0; // first index on Y
+        start[2] = 0; // first index on Z
 
-    imageType::Pointer image_2 = imageType::New();
-    imageType::IndexType start_2;
-    start_2[0] = 0; // first index on X
-    start_2[1] = 0; // first index on Y
-    start_2[2] = 0; // first index on Z
+        imageType::SizeType size;
+        size[0] = width; // size along X
+        size[1] = height; // size along Y
+        size[2] = 1; // size along Z
 
-    imageType::SizeType size_2;
-    size_2[0] = width; // size along X
-    size_2[1] = height; // size along Y
-    size_2[2] = 1; // size along Z
+        imageType::RegionType region_1;
+        region_1.SetSize(size);
+        region_1.SetIndex(start);
 
-    imageType::RegionType region_2;
-    region_2.SetSize(size_2);
-    region_2.SetIndex(start_2);
+        image->SetRegions(region_1);
+        image->SetDirection(image_orientation);
+        image->SetOrigin(image_origin);
+        image->SetSpacing(spacing);
+        image->Allocate();
 
-    image_origin[0] = 0.0;
-	image_origin[1] = 0.0;
-	image_origin[2] = image_origin[2]+ x_offset;
+        image_origin[0] = 0.0;
+	    image_origin[1] = 0.0;
+	    image_origin[2] = image_origin[2] + offset;
 
-    image_2->SetRegions(region_2);
-    image_2->SetDirection(image_orientation);
-    image_2->SetOrigin(image_origin);
-    image_2->SetSpacing(spacing);
-    image_2->Allocate();
-
-    pointer = image_2->GetBufferPointer();
-    std::memcpy(pointer,pixel,sizeof(curan::image::char_pixel_type)*width*height);
-
-    imageType::Pointer image_3 = imageType::New();
-    imageType::IndexType start_3;
-    start_3[0] = 0; // first index on X
-    start_3[1] = 0; // first index on Y
-    start_3[2] = 0; // first index on Z
-
-    imageType::SizeType size_3;
-    size_3[0] = width; // size along X
-    size_3[1] = height; // size along Y
-    size_3[2] = 1; // size along Z
-
-    imageType::RegionType region_3;
-    region_3.SetSize(size_3);
-    region_3.SetIndex(start_3);
-
-    image_origin[0] = 0.0;
-	image_origin[1] = 0.0;
-	image_origin[2] = image_origin[2]+ x_offset;
-
-    image_3->SetRegions(region_3);
-    image_3->SetDirection(image_orientation);
-    image_3->SetOrigin(image_origin);
-    image_3->SetSpacing(spacing);
-    image_3->Allocate();
-
-    pointer = image_3->GetBufferPointer();
-    std::memcpy(pointer,pixel,sizeof(curan::image::char_pixel_type)*width*height);
-
-    desired_images.push_back(image_1);
-    desired_images.push_back(image_2);
-    desired_images.push_back(image_3);
+        auto pointer = image->GetBufferPointer();
+        std::memcpy(pointer,pixel,sizeof(curan::image::char_pixel_type)*width*height);
+        desired_images.push_back(image);
+    }
 }
 
 void create_array_of_linear_images_in_y_direction(std::vector<imageType::Pointer>& desired_images){
@@ -182,21 +130,16 @@ void create_array_of_rotational_images_around_the_z_axis(std::vector<imageType::
 
 
 }
-
+//void volume_creation(curan::renderable::Window& window){
 void volume_creation(curan::renderable::Window& window){
-
+try{
     std::vector<imageType::Pointer> images;
     create_array_of_linear_images_in_x_direction(images);
 
 	curan::image::VolumeReconstructor reconstructor;
 
-    itk::Vector<double,3> spacing;
-    spacing[0] = 1.0;
-    spacing[1] = 1.0;
-    spacing[2] = 1.0;
-
     std::array<double,2> clip_origin = {0.0,0.0};
-    std::array<double,2> clip_size = {3.0,3.0};
+    std::array<double,2> clip_size = {width,height};
 
 	reconstructor.set_output_spacing(spacing);
 	reconstructor.set_fill_strategy(curan::image::VolumeReconstructor::FillingStrategy::GAUSSIAN);
@@ -210,7 +153,7 @@ void volume_creation(curan::renderable::Window& window){
 	std::cout << "Started volumetric filling: \n";
 	curan::image::VolumeReconstructor::KernelDescriptor descript;
 	descript.fillType = curan::image::VolumeReconstructor::FillingStrategy::DISTANCE_WEIGHT_INVERSE;
-	descript.size = 5;
+    descript.size = 5;
 	descript.stdev = 1;
 	descript.minRatio = 0.1;
 	reconstructor.add_kernel_descritor(descript);
@@ -221,14 +164,16 @@ void volume_creation(curan::renderable::Window& window){
 	reconstructor.get_output_pointer(buffer);
 
     auto size = buffer->GetLargestPossibleRegion().GetSize();
-
+    std::printf("size : ");
+    std::cout << size << std::endl;
+    
     curan::renderable::Volume::Info volumeinfo;
     volumeinfo.width = size.GetSize()[0]; 
     volumeinfo.height = size.GetSize()[1];
     volumeinfo.depth = size.GetSize()[2];
-    volumeinfo.spacing_x = spacing[0];
-    volumeinfo.spacing_y = spacing[1];
-    volumeinfo.spacing_z = spacing[2];
+    volumeinfo.spacing_x = final_spacing[0];
+    volumeinfo.spacing_y = final_spacing[1];
+    volumeinfo.spacing_z = final_spacing[2];
     
     auto volume = curan::renderable::Volume::make(volumeinfo);
     window << volume;
@@ -239,8 +184,14 @@ void volume_creation(curan::renderable::Window& window){
     };
     casted_volume->update_volume(updater);
 }
+catch(std::exception & e){
+    std::cout << "failed : " << e.what() << std::endl;
+}
+}
 
 int main(){
+try{
+    //volume_creation();    
     curan::renderable::Window::Info info;
     info.api_dump = false;
     info.display = "";
@@ -263,5 +214,8 @@ int main(){
                     std::cout << "Object contained: " << p.first << '\n';
                 }
     });
-    return 0;
+} catch(std::exception & e){
+    std::cout << "failed: " << e.what() << std::endl;
+}
+return 0;
 }
