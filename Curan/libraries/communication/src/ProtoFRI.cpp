03b5feb024c765396b550fca0fbb4ea06da91dc1
João Oliveira <44142506+Joaopmoliveira@users.noreply.github.com>
2023-07-22 18:00:22 +0100

    Update ProtoFRI.cpp
    changing ProtoFRI to accomodate new API

--- Curan/libraries/communication/src/ProtoFRI.cpp
+++ Curan/libraries/communication/src/ProtoFRI.cpp
@@ -14,9 +14,8 @@ FRIClientConnection::FRIClientConnection(Client* supplied_owner) : owner{ suppli
 
 void read_header_first_time(FRIClientConnection val) {
 	asio::async_read(val.owner->get_socket().get_underlying_socket(),
-		asio::buffer(val.get_buffer(), val.get_header_size()), asio::transfer_all(),
+		asio::buffer(val.message.get_buffer(), val.message.get_header_size()), asio::transfer_all(),
 		[val](std::error_code ec, std::size_t len) {
-        val
 		if (!ec) {
 			read_body(val, ec);
 		} else {
@@ -31,37 +30,30 @@ void read_header(FRIClientConnection val, std::error_code ec) {
 	//we have a message fully unpacked in memory that we must broadcast to all
 	//listeners of the interface. We do this by calling the templated broadcast method
 	size_t temp = 0;
-
-	val.owner->transverse_callables<interface_fri>(temp, ec, val.message_to_receive);
-
-	val.header_to_receive = igtl::MessageHeader::New();
-	val.header_to_receive->InitPack();
-	val.message_to_receive = igtl::MessageBase::New();
+    val.message.deserialize();
+	val.owner->transverse_callables<interface_fri>(temp, ec, val.message);
 	asio::async_read(val.owner->get_socket().get_underlying_socket(),
-		asio::buffer(val.header_to_receive->GetPackPointer(), val.header_to_receive->GetPackSize()), asio::transfer_all(),
+		asio::buffer(val.message.get_buffer(), val.message.get_header_size()), asio::transfer_all(),
 		[val](std::error_code ec, std::size_t len){
 		if (!ec) {
 			read_body(val, ec);
 		} else {
-			val.owner->transverse_callables<interface_fri>(0, ec, val.message_to_receive);
+			val.owner->transverse_callables<interface_fri>(0, ec, val.message);
 			val.owner->get_socket().get_underlying_socket().close();
 		}}
 	);
 }
 
 void read_body(FRIClientConnection val, std::error_code ec) {
-	val.header_to_receive->Unpack();
-	val.message_to_receive = igtl::MessageBase::New();
-	val.message_to_receive->SetMessageHeader(val.header_to_receive);
-	val.message_to_receive->AllocatePack();
+	val.message.deserialize_header();
 
 	asio::async_read(val.owner->get_socket().get_underlying_socket(),
-		asio::buffer(val.message_to_receive->GetPackBodyPointer(), val.message_to_receive->GetPackBodySize()), asio::transfer_all(),
+		asio::buffer(val.message.get_body_buffer(), val.message.get_body_size()), asio::transfer_all(),
 		[val](std::error_code ec, std::size_t len){
 		if (!ec)
 			read_header(val, ec);
 		else {
-			val.owner->transverse_callables<interface_fri>(0, ec, val.message_to_receive);
+			val.owner->transverse_callables<interface_fri>(0, ec, val.message);
 			val.owner->get_socket().get_underlying_socket().close();
 		}}
 	);
