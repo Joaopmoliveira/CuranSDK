ca6c43f3d7e0399c0ba3551116d0079e6a0fac54
João Oliveira <44142506+Joaopmoliveira@users.noreply.github.com>
2023-07-29 00:52:53 +0100

    Erase all static volume specifiers besides the best solution

--- Curan/applications/static_volume_specification/robot_box_with_ultrasound_buzzer/SharedRobotState.cpp
+++
@@ -1,9 +0,0 @@
-#include "SharedRobotState.h"
-
-SharedRobotState::SharedRobotState() : commit_senpuko(false){
-
-}
-
-std::shared_ptr<SharedRobotState> SharedRobotState::make_shared(){
-    return std::shared_ptr<SharedRobotState>(new SharedRobotState());
-}
