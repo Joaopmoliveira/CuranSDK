aaefa04c2aa7ad17702fda75edd39080e77a3017
João Oliveira <44142506+Joaopmoliveira@users.noreply.github.com>
2023-07-29 16:41:41 +0100

    Add HeightField to our 3D rendering capabilities

---
+++ Curan/libraries/rendering/include/rendering/DynamicHeight.h
@@ -0,0 +1,40 @@
+#ifndef CURAN_DYNAMIC_HEIGHT_RENDERABLE_HEADER_FILE_
+#define CURAN_DYNAMIC_HEIGHT_RENDERABLE_HEADER_FILE_
+
+#include <vsg/all.h>
+#include <vsgXchange/all.h>
+#include "Renderable.h"
+#include <optional>
+#include <array>
+
+namespace curan {
+    namespace renderable {
+        struct DynamicHeight : public vsg::Inherit<Renderable, DynamicHeight> {
+
+            size_t width;
+            size_t height;
+            vsg::ref_ptr<vsg::Data> textureData;
+
+            struct Info {
+                size_t width = 100;
+                size_t height = 100;
+                std::array<double,3> origin;
+                std::array<double,3> spacing; 
+                vsg::StateInfo stateInfo;
+                vsg::ref_ptr<vsg::Builder> builder;
+                std::optional<std::string> identifier;
+            };
+
+            DynamicHeight(Info& info);
+
+            static vsg::ref_ptr<Renderable> make(Info& info);
+
+            using updater = std::function<void(vsg::floatArray2D& image)>;
+
+            void update_texture(updater&& update);
+        };
+
+    }
+}
+
+#endif
