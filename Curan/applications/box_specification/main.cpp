396373dd9ea3382d1cb8e626b145982868d685fd
Manuel-Lourenco-Carvalho <44142506+Joaopmoliveira@users.noreply.github.com>
2023-10-01 01:29:53 +0100

    Update main.cpp

--- Curan/applications/box_specification/main.cpp
+++ Curan/applications/box_specification/main.cpp
@@ -1,40 +1,3 @@
-/**
-
-DISCLAIMER OF WARRANTY
-
-The Software is provided "AS IS" and "WITH ALL FAULTS," 
-without warranty of any kind, including without limitation the warranties 
-of merchantability, fitness for a particular purpose and non-infringement. 
-KUKA makes no warranty that the Software is free of defects or is suitable 
-for any particular purpose. In no event shall KUKA be responsible for loss 
-or damages arising from the installation or use of the Software, 
-including but not limited to any indirect, punitive, special, incidental 
-or consequential damages of any character including, without limitation, 
-damages for loss of goodwill, work stoppage, computer failure or malfunction, 
-or any and all other commercial damages or losses. 
-The entire risk to the quality and performance of the Software is not borne by KUKA. 
-Should the Software prove defective, KUKA is not liable for the entire cost 
-of any service and repair.
-
-
-COPYRIGHT
-
-All Rights Reserved
-Copyright (C)  2014-2015 
-KUKA Roboter GmbH
-Augsburg, Germany
-
-This material is the exclusive property of KUKA Roboter GmbH and must be returned 
-to KUKA Roboter GmbH immediately upon request.  
-This material and the information illustrated or contained herein may not be used, 
-reproduced, stored in a retrieval system, or transmitted in whole 
-or in part in any way - electronic, mechanical, photocopying, recording, 
-or otherwise, without the prior written consent of KUKA Roboter GmbH.  
-
-\file
-\version {1.9}
-*/
-
 #include <fstream>
 #include <csignal>
 #include <chrono>
@@ -43,9 +6,6 @@ or otherwise, without the prior written consent of KUKA Roboter GmbH.
 #include <nlohmann/json.hpp>
 #include <cmath>
 #include "utils/Reader.h"
-
-// Variable with the default ID of the robotic system
-constexpr size_t portID = 30200;
 
 namespace
 {
