c08531893df57b364e7ad914c71311c2738c1613
João Oliveira <44142506+Joaopmoliveira@users.noreply.github.com>
2023-12-11 00:31:00 +0000

    Update ImageDisplay.cpp

--- Curan/libraries/userinterface/src/ImageDisplay.cpp
+++ Curan/libraries/userinterface/src/ImageDisplay.cpp
@@ -6,17 +6,10 @@
 namespace curan {
 namespace ui {
 
-ImageWrapper::ImageWrapper(std::shared_ptr<utilities::MemoryBuffer> in_buffer,size_t width,size_t height,SkColorType color, SkAlphaType opaqueness):
- buffer{in_buffer}{
-		image_info = SkImageInfo::Make(width, height,color,opaqueness);
-	    size_t row_size = width * sizeof(char);
-	    auto wraped_skia_pixmap = SkPixmap{image_info,in_buffer->begin()->data(),row_size};
-		image = SkSurfaces::WrapPixels(wraped_skia_pixmap)->makeImageSnapshot();
-}
-
 ImageDisplay::ImageDisplay()  {
 
 }
+
 
 std::unique_ptr<ImageDisplay> ImageDisplay::make() {
 	std::unique_ptr<ImageDisplay> image_display = std::unique_ptr<ImageDisplay>( new ImageDisplay());
