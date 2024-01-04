ccf03b3a9c223e35c7305d8cac43a37e65c35168
Manuel-Lourenco-Carvalho <manuel.l.a.carvalho@tecnico.ulisboa.pt>
2024-01-04 18:48:33 +0000

    Update real_time_extract_oblique_slice.cpp

--- Curan/tests/imageprocessing/real_time_extract_oblique_slice.cpp
+++ Curan/tests/imageprocessing/real_time_extract_oblique_slice.cpp
@@ -1,9 +1,9 @@
 #include "itkImage.h"
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
- 
+
 #include "itkResampleImageFilter.h"
- 
+
 #include "itkEuler3DTransform.h"
 #include "itkNearestNeighborInterpolateImageFunction.h"
 #include "userinterface/widgets/ConfigDraw.h"
@@ -26,7 +26,6 @@
 #include "rendering/DynamicTexture.h"
 #include <optional>
 
-
 constexpr unsigned int Dimension_in = 3;
 constexpr unsigned int Dimension_out = 3;
 using InputPixelType = unsigned char;
@@ -37,21 +36,18 @@ using InterPixelType = float;
 using InputImageType = itk::Image<InterPixelType, Dimension_in>;
 using OutputImageType = itk::Image<OutputPixelType, Dimension_out>;
 
-
 using InterImageType = itk::Image<InterPixelType, Dimension_out>;
 
-//using TransformType = itk::AffineTransform<double, Dimension_in>;
+// using TransformType = itk::AffineTransform<double, Dimension_in>;
 using TransformType = itk::Euler3DTransform<double>;
-
-
 
 using ReaderType = itk::ImageFileReader<InputImageType>;
 using WriterType = itk::ImageFileWriter<OutputImageType>;
 
-
 void updateBaseTexture2D(vsg::vec4Array2D &image, OutputImageType::Pointer image_to_render)
 {
-    try{
+    try
+    {
         int x, y, z;
         auto input = image_to_render;
         auto size = input->GetLargestPossibleRegion().GetSize();
@@ -59,15 +55,15 @@ void updateBaseTexture2D(vsg::vec4Array2D &image, OutputImageType::Pointer image
         y = size[1];
         z = size[2];
 
-        unsigned char* scaller_buffer = (unsigned char*)image_to_render->GetBufferPointer();
-        
+        unsigned char *scaller_buffer = (unsigned char *)image_to_render->GetBufferPointer();
+
         for (size_t r = 0; r < image.height(); ++r)
         {
             using value_type = typename vsg::vec4Array2D::value_type;
-            value_type* ptr = &image.at(0, r);
+            value_type *ptr = &image.at(0, r);
             for (size_t c = 0; c < image.width(); ++c)
             {
-                auto val = *scaller_buffer/255.0;
+                auto val = *scaller_buffer / 255.0;
                 ptr->r = val;
                 ptr->g = val;
                 ptr->b = val;
@@ -76,9 +72,11 @@ void updateBaseTexture2D(vsg::vec4Array2D &image, OutputImageType::Pointer image
                 ++scaller_buffer;
             }
         }
-
-    } catch(std::exception& e){
-        std::cout << "exception : " << e.what() << std::endl;;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "exception : " << e.what() << std::endl;
+        ;
     }
 }
 
@@ -114,97 +112,148 @@ void updateBaseTexture3D(vsg::floatArray3D &image, InputImageType::Pointer image
         image.set(idx[0], idx[1], idx[2], outputIt.Get());
     }
 }
- 
 
- 
-int 
-main(int argc, char * argv[]) {
- 
-  auto reader = ReaderType::New();
-  auto writer = WriterType::New();
- 
-  std::string dirName_input{CURAN_COPIED_RESOURCE_PATH "/precious_phantom/precious_phantom.mha"};
-  std::string dirName_output{"extracted_slice.mha"};
+void resampler(curan::renderable::DynamicTexture *texture, InputImageType::Pointer volume, itk::Point<double> image_origin, itk::Matrix<double> image_orientation)
+{
+    using FilterType = itk::ResampleImageFilter<InputImageType, OutputImageType>;
+    auto filter = FilterType::New();
 
-  reader->SetFileName(dirName_input);
-  writer->SetFileName(dirName_output);
+    // using InterpolatorType = itk::LinearInterpolateImageFunction<InputImageType, double>;
+    using InterpolatorType = itk::NearestNeighborInterpolateImageFunction<InputImageType, double>;
+    auto interpolator = InterpolatorType::New();
+    filter->SetInterpolator(interpolator);
+    filter->SetDefaultPixelValue(300);
 
-  try
+    auto input = volume;
+    auto size = input->GetLargestPossibleRegion().GetSize();
+    auto spacing = input->GetSpacing();
+    double minimum_spacing = std::min(std::min(spacing[0], spacing[1]), spacing[2]);
+    double maximum_size = std::max(std::max(size[0], size[1]), size[2]);
+    auto new_spacing = spacing;
+    new_spacing[0] = minimum_spacing;
+    new_spacing[1] = minimum_spacing;
+    new_spacing[2] = 0.00001;
+
+    auto out_size = size;
+    out_size[0] = maximum_size;
+    out_size[1] = maximum_size;
+    out_size[2] = 1;
+
+    filter->SetInput(input);
+
+    filter->SetSize(out_size);
+
+    auto old_origin = volume->GetOrigin();
+
+    filter->SetOutputSpacing(new_spacing);
+
+    filter->SetOutputDirection(volume->GetDirection());
+    filter->SetOutputOrigin(old_origin);
+
+    TransformType::Pointer transform = TransformType::New();
+
+    itk::Point<double, 3> rotation_center;
+    rotation_center[0] = old_origin[0] + spacing[0] * size[0] / 2.0;
+    rotation_center[1] = old_origin[1] + spacing[1] * size[1] / 2.0;
+    rotation_center[2] = old_origin[2] + spacing[2] * size[2] / 2.0;
+
+    transform->SetCenter(rotation_center);
+    transform->SetRotation(0.0, 0.0, 0.0);
+
+    TransformType::OutputVectorType translation;
+    translation[0] = image_origin[0]; // X translation in millimeters
+    translation[1] = image_origin[1]; // Y translation in millimeters
+    translation[2] = image_origin[2]; // Y translation in millimeters
+    transform->SetTranslation(translation);
+
+    TransformType::MatrixType matrix = transform->GetMatrix();
+    TransformType::OffsetType offset = transform->GetOffset();
+
+    filter->SetTransform(transform);
+
+    try
+    {
+        filter->Update();
+    }
+    catch (const itk::ExceptionObject &e)
+    {
+        std::string result = "Failure to update the filter" + std::string{e.what()};
+        std::cout << result;
+        // return;
+    }
+
+    OutputImageType::Pointer output = filter->GetOutput();
+
+    auto image_direction = output->GetDirection();
+    // auto image_origin = output->GetOrigin();
+    auto image_homogenenous_transformation = vsg::translate(0.0, 0.0, 0.0);
+    image_homogenenous_transformation(3, 0) = (old_origin[0] + translation[0]) / 1000.0;
+    image_homogenenous_transformation(3, 1) = (old_origin[1] + translation[1]) / 1000.0;
+    image_homogenenous_transformation(3, 2) = (old_origin[2] + translation[2]) / 1000.0;
+
+    for (size_t row = 0; row < 3; ++row)
+        for (size_t col = 0; col < 3; ++col)
+            image_homogenenous_transformation(col, row) = image_direction(row, col);
+
+    texture->update_transform(image_homogenenous_transformation);
+
+    auto updater_image = [output](vsg::vec4Array2D &image)
+    { updateBaseTexture2D(image, output); };
+    texture->update_texture(updater_image);
+}
+
+int main(int argc, char *argv[])
+{
+
+    auto reader = ReaderType::New();
+    auto writer = WriterType::New();
+
+    std::string dirName_input{CURAN_COPIED_RESOURCE_PATH "/precious_phantom/precious_phantom.mha"};
+    std::string dirName_output{"extracted_slice.mha"};
+
+    reader->SetFileName(dirName_input);
+    writer->SetFileName(dirName_output);
+
+    try
     {
         reader->Update();
     }
     catch (const itk::ExceptionObject &ex)
     {
         std::cout << ex << std::endl;
-        //return;
+        // return;
     }
- 
 
-  InputImageType::Pointer pointer_to_block_of_memory = reader->GetOutput();
-  InputImageType::SizeType size_itk = pointer_to_block_of_memory->GetLargestPossibleRegion().GetSize();
+    InputImageType::Pointer pointer_to_block_of_memory = reader->GetOutput();
+    InputImageType::SizeType size_itk = pointer_to_block_of_memory->GetLargestPossibleRegion().GetSize();
 
-  std::cout << "(input) origin : " << pointer_to_block_of_memory->GetOrigin() << std::endl;
-  std::cout << "(input) size : " << pointer_to_block_of_memory->GetLargestPossibleRegion().GetSize() << std::endl;
-  std::cout << "(input) spacing : " << pointer_to_block_of_memory->GetSpacing() << std::endl;
-  std::cout << "(input) direction : " << pointer_to_block_of_memory->GetDirection() << std::endl;
+    auto input = pointer_to_block_of_memory;
+    auto size = input->GetLargestPossibleRegion().GetSize();
+    auto spacing = input->GetSpacing();
 
-  using FilterType = itk::ResampleImageFilter<InputImageType, OutputImageType>;
-  auto filter = FilterType::New();
+    curan::renderable::Window::Info info;
+    info.api_dump = false;
+    info.display = "";
+    info.full_screen = false;
+    info.is_debug = false;
+    info.screen_number = 0;
+    info.title = "myviewer";
+    curan::renderable::Window::WindowSize size_1{1000, 800};
+    info.window_size = size_1;
+    curan::renderable::Window window{info};
 
-  //using InterpolatorType = itk::LinearInterpolateImageFunction<InputImageType, double>;
-  using InterpolatorType = itk::NearestNeighborInterpolateImageFunction<InputImageType, double>;
-  auto interpolator = InterpolatorType::New();
-  filter->SetInterpolator(interpolator);
-  filter->SetDefaultPixelValue(300);
+    curan::renderable::Volume::Info volumeinfo;
+    volumeinfo.width = size.GetSize()[0];
+    volumeinfo.height = size.GetSize()[1];
+    volumeinfo.depth = size.GetSize()[2];
+    volumeinfo.spacing_x = spacing[0];
+    volumeinfo.spacing_y = spacing[1];
+    volumeinfo.spacing_z = spacing[2];
 
-  auto input = pointer_to_block_of_memory;
-  auto size = input->GetLargestPossibleRegion().GetSize();
-  auto spacing = input->GetSpacing();
-  double minimum_spacing = std::min(std::min(spacing[0], spacing[1]), spacing[2]);
-  double maximum_size = std::max(std::max(size[0], size[1]), size[2]);
-  auto new_spacing = spacing;
-  new_spacing[0] = minimum_spacing;
-  new_spacing[1] = minimum_spacing;
-  new_spacing[2] = 0.00001;
+    auto volume_to_render = curan::renderable::Volume::make(volumeinfo);
+    window << volume_to_render;
 
-  auto out_size = size;
-  out_size[0] = maximum_size;
-  out_size[1] = maximum_size;
-  out_size[2] = maximum_size;
-
-  out_size[2] = 1;
-  
-  
-  
-  filter->SetInput(input);
-
-  filter->SetSize(out_size);
-
-  auto old_origin = pointer_to_block_of_memory->GetOrigin() ;
-
-  curan::renderable::Window::Info info;
-  info.api_dump = false;
-  info.display = "";
-  info.full_screen = false;
-  info.is_debug = false;
-  info.screen_number = 0;
-  info.title = "myviewer";
-  curan::renderable::Window::WindowSize size_1{1000, 800};
-  info.window_size = size_1;
-  curan::renderable::Window window{info};
-
-  curan::renderable::Volume::Info volumeinfo;
-  volumeinfo.width = size.GetSize()[0];
-  volumeinfo.height = size.GetSize()[1];
-  volumeinfo.depth = size.GetSize()[2];
-  volumeinfo.spacing_x = spacing[0];
-  volumeinfo.spacing_y = spacing[1];
-  volumeinfo.spacing_z = spacing[2];
-
-  auto volume_to_render = curan::renderable::Volume::make(volumeinfo);
-  window << volume_to_render;
-
-  auto direction = pointer_to_block_of_memory->GetDirection();
+    auto direction = pointer_to_block_of_memory->GetDirection();
     auto origin = pointer_to_block_of_memory->GetOrigin();
     auto fixed_homogenenous_transformation = vsg::translate(0.0, 0.0, 0.0);
     fixed_homogenenous_transformation(3, 0) = origin[0] / 1000.0;
@@ -222,116 +271,56 @@ main(int argc, char * argv[]) {
     { updateBaseTexture3D(image, pointer_to_block_of_memory); };
     casted_volume_fixed->update_volume(updater);
 
-  
-  filter->SetOutputSpacing(new_spacing);
-  itk::Matrix<double,3,3> new_direction;
+    curan::renderable::DynamicTexture::Info infotexture;
 
+    auto size_2 = input->GetLargestPossibleRegion().GetSize();
+    auto spacing_2 = input->GetSpacing();
+    double minimum_spacing = std::min(std::min(spacing_2[0], spacing_2[1]), spacing_2[2]);
+    double maximum_size = std::max(std::max(size_2[0], size_2[1]), size_2[2]);
+    auto new_spacing = spacing_2;
+    new_spacing[0] = minimum_spacing;
+    new_spacing[1] = minimum_spacing;
+    new_spacing[2] = 0.00001;
 
-  double angle = 0.0;
-  new_direction(0,0) = std::cos(angle);
-  new_direction(1,0) = std::sin(angle);
-  new_direction(2,0) = 0.0;
+    auto out_size = size_2;
+    out_size[0] = maximum_size;
+    out_size[1] = maximum_size;
+    out_size[2] = 1;
 
-  new_direction(0,1) = -std::sin(angle);
-  new_direction(1,1) =  std::cos(angle);
-  new_direction(2,1) = 0.0;
+    infotexture.height = out_size[1];
+    infotexture.width = out_size[0];
+    infotexture.spacing = {new_spacing[0] / 1000, new_spacing[1] / 1000, new_spacing[2] / 1000};
+    infotexture.origin = {0.0, 0.0, 0.0};
+    infotexture.builder = vsg::Builder::create();
 
-  new_direction(0,2) = 0.0;
-  new_direction(1,2) = 0.0;
-  new_direction(2,2) = 1.0;
-  filter->SetOutputDirection(pointer_to_block_of_memory->GetDirection()*new_direction);
+    auto dynamic_texture = curan::renderable::DynamicTexture::make(infotexture);
+    window << dynamic_texture;
+    auto casted_image = dynamic_texture->cast<curan::renderable::DynamicTexture>();
 
-  
-  TransformType::Pointer transform = TransformType::New();
+    std::atomic<bool> continue_running = true;
 
-  itk::Point<double, 3> rotation_center;
-  rotation_center[0] = old_origin[0] + spacing[0] * size[0] / 2.0;
-  rotation_center[1] = old_origin[1] + spacing[1] * size[1] / 2.0;
-  rotation_center[2] = old_origin[2] + spacing[2] * size[2] / 2.0;
+    std::thread run_slice_extractor{[&]()
+                                    {
+                                        while (continue_running)
+                                        {
+                                            itk::Point<double> image_origin;
+                                            itk::Matrix<double> image_orientation;
+                                            for (size_t zzz = 0; zzz < 200; ++zzz)
+                                            {
+                                                image_origin[0] = 0;
+                                                image_origin[1] = 0;
+                                                image_origin[2] = zzz;
+                                                resampler(casted_image, pointer_to_block_of_memory, image_origin, image_orientation);
+                                            }
+                                        }
+                                    }};
 
-  transform->SetCenter(rotation_center);
-  transform->SetRotation(0.0,0.0,0.0);
+    window.run();
+    continue_running = false;
+    run_slice_extractor.join();
 
-  TransformType::OutputVectorType translation;
-  translation[0] = 0; // X translation in millimeters
-  translation[1] = 0; // Y translation in millimeters
-  translation[2] = 100; // Y translation in millimeters
-  transform->SetTranslation(translation);
-  
+    /* writer->SetInput(output);
+    writer->Update(); */
 
-  itk::Point<double,3>::VectorType new_origin{{0.0,0.0,0.0}};
-  old_origin += new_origin;
-  filter->SetOutputOrigin(old_origin);
-
-
-  TransformType::MatrixType matrix = transform->GetMatrix();
-  TransformType::OffsetType offset = transform->GetOffset();
-
-  std::cout << "transformation rotation matrix: \n" << matrix << std::endl;
-
-  std::cout << "transformation translation vector: \n" << offset << "\n" << std::endl;
-  
-  filter->SetTransform(transform);
-
-
-
-  try
-  {
-      filter->Update();
-  }
-  catch (const itk::ExceptionObject& e)
-  {
-      std::string result = "Failure to update the filter"+std::string{e.what()};
-      std::cout << result;
-      //return;
-  }
-
-  OutputImageType::Pointer output = filter->GetOutput();
-
-
-
-
-  curan::renderable::DynamicTexture::Info infotexture;
-  auto output_image = output;
-  auto output_image_size = output_image->GetLargestPossibleRegion().GetSize();
-  auto output_image_spacing = output_image->GetSpacing();
-  infotexture.height = output_image_size[1];
-  infotexture.width = output_image_size[0];
-  infotexture.spacing = {output_image_spacing[0]/1000,output_image_spacing[1]/1000,output_image_spacing[2]/1000};
-  infotexture.origin = {0.0,0.0,0.0};
-  infotexture.builder = vsg::Builder::create();
-
-  auto dynamic_texture = curan::renderable::DynamicTexture::make(infotexture);
-  window << dynamic_texture;
-
-  auto image_direction = output->GetDirection();
-  auto image_origin = output->GetOrigin();
-  auto image_homogenenous_transformation = vsg::translate(0.0, 0.0, 0.0);
-  image_homogenenous_transformation(3, 0) = (image_origin[0] + translation[0]) / 1000.0;
-  image_homogenenous_transformation(3, 1) = (image_origin[1] + translation[1]) / 1000.0;
-  image_homogenenous_transformation(3, 2) = (image_origin[2] + translation[2]) / 1000.0;
-
-  for (size_t row = 0; row < 3; ++row)
-      for (size_t col = 0; col < 3; ++col)
-          image_homogenenous_transformation(col, row) = image_direction(row, col);
-
-  dynamic_texture->cast<curan::renderable::DynamicTexture>()->update_transform(image_homogenenous_transformation);
-
-  auto casted_image = dynamic_texture->cast<curan::renderable::DynamicTexture>();
-  auto updater_image = [output](vsg::vec4Array2D &image)
-    { updateBaseTexture2D(image, output); };
-  casted_image->update_texture(updater_image);
-
-
-  std::cout << "(output) origin : " << output->GetOrigin() << std::endl;
-  std::cout << "(output) size : " << output->GetLargestPossibleRegion().GetSize() << std::endl;
-  std::cout << "(output) spacing : " << output->GetSpacing() << std::endl;
-  std::cout << "(output) direction : " << output->GetDirection() << std::endl;
- 
-  window.run();
-
-  writer->SetInput(output);
-  writer->Update();
- 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
