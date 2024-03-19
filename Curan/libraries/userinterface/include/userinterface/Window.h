c27507decd7ab9c9b9b23e79421d507e2e7d5dac
João Oliveira <44142506+Joaopmoliveira@users.noreply.github.com>
2024-03-19 17:21:50 +0000

    Update Window.h

--- Curan/libraries/userinterface/include/userinterface/Window.h
+++ Curan/libraries/userinterface/include/userinterface/Window.h
@@ -10,7 +10,67 @@ namespace curan {
 namespace ui {
 
 /*
-The Window class is by far on the more complicated side in terms of machinery used to control the interface between the operating system and our code. 
+The Window class is by far on the more complicated 
+side in terms of machinery used to control the 
+interface between the operating system and our code. 
+
+The usual loop to control a windows goes like 
+this (comments start with % keyword): 
+
+IconResources resources{CURAN_COPIED_RESOURCE_PATH"/images"};
+
+% The IconResources is a class which loads icons to 
+% be used by buttons, etc... 
+% For it to load we need to pass a directory where these widgets are located
+
+std::unique_ptr<Context> context = std::make_unique<Context>();
+
+% The context contains the bridge between the GPU 
+% context and our vulkan code, which uses SKIA to 
+% generate drawing calls to the vulkan pipeline
+
+DisplayParams param{ std::move(context),2200,1800 };
+std::unique_ptr<Window> viewer = std::make_unique<Window>(std::move(param));
+
+% Now we have a window, which we can use to 
+% obtain mouse events, and a swapchain to 
+% which we can draw into
+% Now comes the trycky part, 
+% we will not go into detail, 
+% but our library can generate drawing logic 
+% which we can then use 
+
+auto page = Page{std::move(widgetcontainer),SK_ColorBLACK};
+page.update_page(viewer.get());
+
+ConfigDraw config_draw{ &page};
+
+viewer->set_minimum_size(page.minimum_size());
+
+while (!glfwWindowShouldClose(viewer->window)) {
+	auto start = std::chrono::high_resolution_clock::now();
+	SkSurface* pointer_to_surface = viewer->getBackbufferSurface();
+	SkCanvas* canvas = pointer_to_surface->getCanvas();
+	if (viewer->was_updated()) {
+		page.update_page(viewer.get());
+		viewer->update_processed();
+	}
+	page.draw(canvas);
+	auto signals = viewer->process_pending_signals();
+
+	if (!signals.empty())
+		page.propagate_signal(signals.back(), &config_draw);
+	page.propagate_heartbeat(&config_draw);
+	glfwPollEvents();
+
+	bool val = viewer->swapBuffers();
+	if (!val)
+		std::cout << "failed to swap buffers\n";
+	auto end = std::chrono::high_resolution_clock::now();
+	std::this_thread::sleep_for(std::chrono::milliseconds(16) - std::chrono::duration_cast<std::chrono::milliseconds>(end - start));
+}
+
+There is alot of machinery in these lines, which we will try to explain as well as possible. 
 */
 
 struct DisplayParams {
