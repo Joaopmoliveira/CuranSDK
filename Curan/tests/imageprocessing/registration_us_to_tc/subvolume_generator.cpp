8d97a21de05c13254e1bd6aca0370d600e0c32fa
Manuel-Lourenco-Carvalho <91229515+Manuel-Lourenco-Carvalho@users.noreply.github.com>
2024-05-06 18:03:47 +0100

    examples build with álvaro

--- Curan/tests/imageprocessing/registration_us_to_tc/subvolume_generator.cpp
+++ Curan/tests/imageprocessing/registration_us_to_tc/subvolume_generator.cpp
@@ -1,47 +1,57 @@
 #include <iostream>
 #include <optional>
 #include <string>
+#include <sstream>
+#include <cassert>
+#include <charconv>
+#include <iomanip>
+#include <iostream>
+#include <optional>
+#include <string_view>
+#include <system_error>
 
-std::optional<FixedImageType::Pointer> read_volume_from_directory(const std::string &path)
-{
-    using ReaderType = itk::ImageSeriesReader<FixedImageType>;
-    auto reader = ReaderType::New();
+#include "itkImage.h"
+#include "itkGDCMImageIO.h"
+#include "itkGDCMSeriesFileNames.h"
+#include "itkImageSeriesReader.h"
+#include "itkImageFileWriter.h"
+#include "itkImageRegionIteratorWithIndex.h"
+#include "itkRescaleIntensityImageFilter.h"
+#include "itkCastImageFilter.h"
+#include "itkImageRegistrationMethodv4.h"
+#include "itkMeanSquaresImageToImageMetricv4.h"
+#include "itkEuler3DTransform.h"
+#include "itkMattesMutualInformationImageToImageMetricv4.h"
+#include "itkNormalizedMutualInformationHistogramImageToImageMetric.h"
+#include "itkVersorRigid3DTransform.h"
+#include "itkCenteredTransformInitializer.h"
+#include "itkRegularStepGradientDescentOptimizerv4.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkResampleImageFilter.h"
+#include "itkCastImageFilter.h"
+#include "itkSubtractImageFilter.h"
+#include "itkRescaleIntensityImageFilter.h"
+#include "itkExtractImageFilter.h"
+#include "itkCommand.h"
+#include "itkImageDuplicator.h"
+#include "itkRegionOfInterestImageFilter.h"
 
-    using ImageIOType = itk::GDCMImageIO;
-    auto dicomIO = ImageIOType::New();
-    dicomIO->LoadSequencesOn();
-    dicomIO->SetMaxSizeLoadEntry(0xffffff);
-    dicomIO->SetKeepOriginalUID(true);
-    reader->SetImageIO(dicomIO);
 
-    using NamesGeneratorType = itk::GDCMSeriesFileNames;
-    auto nameGenerator = NamesGeneratorType::New();
+using PixelType = float;
+constexpr unsigned int Dimension = 3;
+using ImageType = itk::Image<PixelType, Dimension>;
+using TransformType = itk::Euler3DTransform<double>;
+using OptimizerType = itk::RegularStepGradientDescentOptimizerv4<double>;
+using MetricType = itk::MattesMutualInformationImageToImageMetricv4<ImageType, ImageType>;
+using RegistrationType = itk::ImageRegistrationMethodv4<ImageType, ImageType, TransformType>;
+using OutputPixelType = unsigned char;
+using OutputImageType = itk::Image<PixelType, Dimension>;
+using CastFilterType = itk::CastImageFilter<ImageType, OutputImageType>;
+using RescaleFilterType = itk::RescaleIntensityImageFilter<ImageType, OutputImageType>;
+using WriterType = itk::ImageFileWriter<OutputImageType>;
+using FixedImageReaderType = itk::ImageFileReader<ImageType>;
 
-    nameGenerator->SetUseSeriesDetails(true);
-    nameGenerator->AddSeriesRestriction("0008|0021");
-
-    nameGenerator->SetDirectory(path);
-
-    using FileNamesContainer = std::vector<std::string>;
-    FileNamesContainer fileNames;
-
-    fileNames = nameGenerator->GetFileNames(nameGenerator->GetSeriesUIDs().front());
-
-    if (fileNames.size() == 0)
-        return std::nullopt;
-
-    reader->SetFileNames(fileNames);
-    try
-    {
-        reader->Update();
-    }
-    catch (const itk::ExceptionObject &ex)
-    {
-        std::cout << ex << std::endl;
-        return std::nullopt;
-    }
-    return reader->GetOutput();
-}
 
 int main(int argc, char* argv[]){
     if(argc!=5){
@@ -53,7 +63,103 @@ int main(int argc, char* argv[]){
     std::string close_to_origin{argv[3]};
     std::string away_from_origin{argv[4]};
 
+    std::vector<int> values_start;
+    {
+    std::istringstream ss(close_to_origin);
+    for (std::string line; std::getline(ss, line,',');){
+        int result{};
+        auto [ptr, ec] = std::from_chars(line.data(), line.data() + line.size(), result);
+        if (ec == std::errc::invalid_argument){
+            std::cout << "This is not a number.\n";
+            return 1;
+        }
+        else if (ec == std::errc::result_out_of_range){
+            std::cout << "This number is larger than an int.\n";
+            return 1;
+        }
+        values_start.push_back(result);
+    }
+    if(values_start.size()!=3){
+        std::cout << "must supply three coordinate values\n";
+        return 1;
+    }
+    }
 
+    std::vector<int> values_end;
+    {
+    std::istringstream ss(away_from_origin);
+    for (std::string line; std::getline(ss, line,',');){
+        int result{};
+        auto [ptr, ec] = std::from_chars(line.data(), line.data() + line.size(), result);
+        if (ec == std::errc::invalid_argument){
+            std::cout << "This is not a number.\n";
+            return 1;
+        }
+        else if (ec == std::errc::result_out_of_range){
+            std::cout << "This number is larger than an int.\n";
+            return 1;
+        }
+        values_end.push_back(result);
+    }
+    if(values_end.size()!=3){
+        std::cout << "must supply three coordinate values\n";
+        return 1;
+    }
+    }
 
+    for(size_t i = 0; i< 3; ++i)
+        if(values_end[i] <= values_start[i]){
+            std::printf("the (%d) ending coordinates (%d) must be larger than the start coordinates (%d)\n",i,values_end[i],values_start[i]);
+            return 1;
+        }
+    
+    
+    auto fixedImageReader = FixedImageReaderType::New();
+    fixedImageReader->SetFileName(input_volume);
+
+    using DuplicatorType = itk::ImageDuplicator<ImageType>;
+    auto duplicator = DuplicatorType::New();
+    duplicator->SetInputImage(fixedImageReader->GetOutput());
+
+    try {
+        duplicator->Update();
+    } catch (const itk::ExceptionObject & error) {
+        std::cerr << "Error: " << error << std::endl;
+        throw std::runtime_error("error");
+    }
+
+    ImageType::Pointer new_image = duplicator->GetOutput();
+
+    ImageType::IndexType start;
+    start[0] = values_start[0];
+    start[1] = values_start[1];
+    start[2] = values_start[2];
+
+    ImageType::SizeType end;
+    end[0] = values_end[0];
+    end[1] = values_end[1];
+    end[2] = values_end[2];
+
+    ImageType::RegionType region;
+    region.SetIndex(start);
+    region.SetSize(end);
+
+    using FilterType = itk::RegionOfInterestImageFilter<ImageType, ImageType>;
+    auto filter = FilterType::New();
+    filter->SetInput(new_image);
+    filter->SetRegionOfInterest(region);
+
+    using WriterType = itk::ImageFileWriter<ImageType>;
+
+    WriterType::Pointer writer = WriterType::New();
+    writer->SetInput(filter->GetOutput());
+    writer->SetFileName(output_volume);
+
+    try {
+       writer->Update();
+    } catch (const itk::ExceptionObject & error) {
+        std::cerr << "Error: " << error << std::endl;
+        throw std::runtime_error("error");
+    }
     return 0;
 }
