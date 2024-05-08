978b355730ce35fa3f039f7dd625bbe250c03173
alvarolopes01 <119494386+alvarolopes01@users.noreply.github.com>
2024-05-08 04:14:37 +0100

    Subvolume generator finished
    The program takes 3 input arguments: the input volume, the start coordinate and the end coordinate. Example:
    ./test_subvolume_generator ultrasound_precious_phantom1.mha  0,100,0 454,244,567
    It uses the post build resource path, so make sure to put the input files in that directory. Then, by default, it creates the output volume in this path and names it according to the choosen coordinates to make the process easy when experimenting with lots of subvolumes.

--- Curan/tests/imageprocessing/registration_us_to_tc/subvolume_generator.cpp
+++ Curan/tests/imageprocessing/registration_us_to_tc/subvolume_generator.cpp
@@ -1,68 +1,29 @@
 #include <iostream>
-#include <optional>
 #include <string>
 #include <sstream>
-#include <cassert>
 #include <charconv>
-#include <iomanip>
-#include <iostream>
-#include <optional>
-#include <string_view>
-#include <system_error>
-
+#include <stdexcept>
 #include "itkImage.h"
-#include "itkGDCMImageIO.h"
-#include "itkGDCMSeriesFileNames.h"
-#include "itkImageSeriesReader.h"
-#include "itkImageFileWriter.h"
-#include "itkImageRegionIteratorWithIndex.h"
-#include "itkRescaleIntensityImageFilter.h"
-#include "itkCastImageFilter.h"
-#include "itkImageRegistrationMethodv4.h"
-#include "itkMeanSquaresImageToImageMetricv4.h"
-#include "itkEuler3DTransform.h"
-#include "itkMattesMutualInformationImageToImageMetricv4.h"
-#include "itkNormalizedMutualInformationHistogramImageToImageMetric.h"
-#include "itkVersorRigid3DTransform.h"
-#include "itkCenteredTransformInitializer.h"
-#include "itkRegularStepGradientDescentOptimizerv4.h"
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
-#include "itkResampleImageFilter.h"
-#include "itkCastImageFilter.h"
-#include "itkSubtractImageFilter.h"
-#include "itkRescaleIntensityImageFilter.h"
-#include "itkExtractImageFilter.h"
-#include "itkCommand.h"
 #include "itkImageDuplicator.h"
 #include "itkRegionOfInterestImageFilter.h"
-
 
 using PixelType = float;
 constexpr unsigned int Dimension = 3;
 using ImageType = itk::Image<PixelType, Dimension>;
-using TransformType = itk::Euler3DTransform<double>;
-using OptimizerType = itk::RegularStepGradientDescentOptimizerv4<double>;
-using MetricType = itk::MattesMutualInformationImageToImageMetricv4<ImageType, ImageType>;
-using RegistrationType = itk::ImageRegistrationMethodv4<ImageType, ImageType, TransformType>;
-using OutputPixelType = unsigned char;
-using OutputImageType = itk::Image<PixelType, Dimension>;
-using CastFilterType = itk::CastImageFilter<ImageType, OutputImageType>;
-using RescaleFilterType = itk::RescaleIntensityImageFilter<ImageType, OutputImageType>;
-using WriterType = itk::ImageFileWriter<OutputImageType>;
+using WriterType = itk::ImageFileWriter<ImageType>;
 using FixedImageReaderType = itk::ImageFileReader<ImageType>;
 
-
 int main(int argc, char* argv[]){
-    if(argc!=5){
-        std::cout << "To run the executable you must provide four arguments";
+    if(argc!=4){
+        std::cout << "To run you must provide the input volume, starting and ending coordinates separated by spaces. Example:precious_phantom.mha 0,0,0 150,254,140\n";
+        return 1;
     }
 
     std::string input_volume{argv[1]};
-    std::string output_volume{argv[2]};
-    std::string close_to_origin{argv[3]};
-    std::string away_from_origin{argv[4]};
-
+    std::string close_to_origin{argv[2]};
+    std::string away_from_origin{argv[3]};
     std::vector<int> values_start;
     {
     std::istringstream ss(close_to_origin);
@@ -80,13 +41,12 @@ int main(int argc, char* argv[]){
         values_start.push_back(result);
     }
     if(values_start.size()!=3){
-        std::cout << "must supply three coordinate values\n";
+        std::cout << "Must supply three coordinate values.\n";
         return 1;
     }
     }
 
     std::vector<int> values_end;
-    {
     std::istringstream ss(away_from_origin);
     for (std::string line; std::getline(ss, line,',');){
         int result{};
@@ -102,24 +62,33 @@ int main(int argc, char* argv[]){
         values_end.push_back(result);
     }
     if(values_end.size()!=3){
-        std::cout << "must supply three coordinate values\n";
+        std::cout << "Must supply three coordinate values.\n";
         return 1;
     }
-    }
-
-    for(size_t i = 0; i< 3; ++i)
+    
+    for(int i = 0; i< 3; ++i){
         if(values_end[i] <= values_start[i]){
             std::printf("the (%d) ending coordinates (%d) must be larger than the start coordinates (%d)\n",i,values_end[i],values_start[i]);
             return 1;
         }
-    
+    }
     
     auto fixedImageReader = FixedImageReaderType::New();
-    fixedImageReader->SetFileName(input_volume);
+    const std::string aux = "/precious_phantom/";
+    const std::string path = CURAN_COPIED_RESOURCE_PATH + aux + input_volume;
+    fixedImageReader->SetFileName(path);
+
+    try {
+        fixedImageReader->Update();
+    } catch (const itk::ExceptionObject & error) {
+        std::cerr << "Error: " << error << std::endl;
+        return EXIT_FAILURE;
+    }
 
     using DuplicatorType = itk::ImageDuplicator<ImageType>;
     auto duplicator = DuplicatorType::New();
     duplicator->SetInputImage(fixedImageReader->GetOutput());
+
 
     try {
         duplicator->Update();
@@ -129,28 +98,42 @@ int main(int argc, char* argv[]){
     }
 
     ImageType::Pointer new_image = duplicator->GetOutput();
+    ImageType::SizeType size_itk = new_image->GetLargestPossibleRegion().GetSize();
+    std::cout << "Original Image size:" << size_itk[0] << "x" << size_itk[1] << "x" << size_itk[2] << std::endl;
 
     ImageType::IndexType start;
     start[0] = values_start[0];
     start[1] = values_start[1];
     start[2] = values_start[2];
 
-    ImageType::SizeType end;
+    ImageType::IndexType end;
     end[0] = values_end[0];
     end[1] = values_end[1];
     end[2] = values_end[2];
 
+    ImageType::SizeType size;
+    size[0] = end[0] - start[0];
+    size[1] = end[1] - start[1];
+    size[2] = end[2] - start[2];
+
     ImageType::RegionType region;
     region.SetIndex(start);
-    region.SetSize(end);
+    region.SetSize(size);
 
     using FilterType = itk::RegionOfInterestImageFilter<ImageType, ImageType>;
     auto filter = FilterType::New();
     filter->SetInput(new_image);
     filter->SetRegionOfInterest(region);
 
-    using WriterType = itk::ImageFileWriter<ImageType>;
+    std::string output_volume = CURAN_COPIED_RESOURCE_PATH + aux + "output_volume_" +
+    std::to_string(values_start[0]) + "," + 
+    std::to_string(values_start[1]) + "," +
+    std::to_string(values_start[2]) + "_" +
+    std::to_string(values_end[0]) + "," +
+    std::to_string(values_end[1]) + "," +
+    std::to_string(values_end[2]) + ".mha";
 
+    using WriterType = itk::ImageFileWriter<ImageType>;
     WriterType::Pointer writer = WriterType::New();
     writer->SetInput(filter->GetOutput());
     writer->SetFileName(output_volume);
@@ -161,5 +144,8 @@ int main(int argc, char* argv[]){
         std::cerr << "Error: " << error << std::endl;
         throw std::runtime_error("error");
     }
+
+    std::cout << "File written to " << output_volume << std::endl;
+
     return 0;
 }
