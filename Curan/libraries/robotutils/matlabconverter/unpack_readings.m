3b4429a090cfdb66bf409986671a4ea2e490f975
João Oliveira <44142506+Joaopmoliveira@users.noreply.github.com>
2024-05-13 09:28:47 +0100

    Update unpack_readings.m

--- Curan/libraries/robotutils/matlabconverter/unpack_readings.m
+++ Curan/libraries/robotutils/matlabconverter/unpack_readings.m
@@ -25,5 +25,4 @@ measurments.userdef = str2num(decoded_information.userdef);
 measurments.userdef2 = str2num(decoded_information.userdef2); 
 measurments.userdef3 = str2num(decoded_information.userdef3); 
 measurments.userdef4 = str2num(decoded_information.userdef4); 
-
 end
