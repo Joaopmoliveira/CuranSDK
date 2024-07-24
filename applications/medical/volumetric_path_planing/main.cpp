dbe5978f0885d0fcd92bd6b6e0a714cd4f23c077
João Oliveira <44142506+Joaopmoliveira@users.noreply.github.com>
2024-07-24 23:09:15 +0100

    Code seems to be mostly running, now we need to bug test to find all the limitations
    This commit makes it so that the volume application prints the processed volume to a file

--- applications/medical/volumetric_path_planing/main.cpp
+++ applications/medical/volumetric_path_planing/main.cpp
@@ -4,18 +4,41 @@
 #include <nlohmann/json.hpp>
 #include <time.h>
 
-void load_all_files_in_directory(Application& app_data,curan::ui::ConfigDraw* drawing_data, std::atomic<bool>& stop_value,std::mutex& mut){
+void load_all_files_in_directory(Application &app_data, curan::ui::ConfigDraw *drawing_data, std::atomic<bool> &stop_value, std::mutex &mut)
+{
     std::vector<std::string> uids_to_load = get_representative_uids(app_data.path);
-    for(const auto& uid : uids_to_load){
-        std::optional<ImageType::Pointer> image = get_representative_series_image(app_data.path,uid);
-        if(stop_value.load())
+    for (const auto &uid : uids_to_load)
+    {
+        std::optional<ImageType::Pointer> image = get_representative_series_image(app_data.path, uid);
+        if (stop_value.load())
             return;
-        
-        if(image) {std::lock_guard<std::mutex> g{mut};app_data.loaded.push_back({*image,uid});}
-        if(stop_value.load())
+
+        if (image)
+        {
+            std::lock_guard<std::mutex> g{mut};
+            app_data.loaded.push_back({*image, uid});
+        }
+        if (stop_value.load())
             return;
     }
 };
+
+template <typename TImage>
+void DeepCopy(typename TImage::Pointer input, typename TImage::Pointer output)
+{
+    output->SetRegions(input->GetLargestPossibleRegion());
+    output->Allocate();
+
+    itk::ImageRegionConstIterator<TImage> inputIterator(input, input->GetLargestPossibleRegion());
+    itk::ImageRegionIterator<TImage> outputIterator(output, output->GetLargestPossibleRegion());
+
+    while (!inputIterator.IsAtEnd())
+    {
+        outputIterator.Set(inputIterator.Get());
+        ++inputIterator;
+        ++outputIterator;
+    }
+}
 
 int main()
 {
@@ -30,7 +53,7 @@ int main()
 
         std::mutex mut;
 
-        Application data_application{resources,CURAN_COPIED_RESOURCE_PATH "/dicom_sample/ST983524",mut};
+        Application data_application{resources, CURAN_COPIED_RESOURCE_PATH "/dicom_sample/ST983524", mut};
 
         curan::ui::Page page{std::move(data_application.main_page()), SK_ColorBLACK};
 
@@ -40,9 +63,10 @@ int main()
 
         std::atomic<bool> function_value = false;
 
-        curan::utilities::Job job{"load files in backend",[&](){
-            load_all_files_in_directory(data_application,&config,function_value,mut);
-        }};
+        curan::utilities::Job job{"load files in backend", [&]()
+                                  {
+                                      load_all_files_in_directory(data_application, &config, function_value, mut);
+                                  }};
         data_application.pool->submit(job);
 
         while (!glfwWindowShouldClose(viewer->window))
@@ -56,7 +80,7 @@ int main()
                 viewer->update_processed();
             }
             page.draw(canvas);
-            for(auto&& signal : viewer->process_pending_signals())
+            for (auto &&signal : viewer->process_pending_signals())
                 page.propagate_signal(signal, &config);
             glfwPollEvents();
 
@@ -67,37 +91,95 @@ int main()
             std::this_thread::sleep_for(std::chrono::milliseconds(16) - std::chrono::duration_cast<std::chrono::milliseconds>(end - start));
         }
 
-	    auto return_current_time_and_date = [](){
-	        auto now = std::chrono::system_clock::now();
-    	    auto in_time_t = std::chrono::system_clock::to_time_t(now);
-    	    std::stringstream ss;
-    	    ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %X");
-   		    return ss.str();
-	    };
+        auto return_current_time_and_date = []()
+        {
+            auto now = std::chrono::system_clock::now();
+            auto in_time_t = std::chrono::system_clock::to_time_t(now);
+            std::stringstream ss;
+            ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %X");
+            return ss.str();
+        };
 
         Eigen::IOFormat CleanFmt(Eigen::StreamPrecision, 0, ", ", "\n", " ", " ");
 
-
-        if(data_application.final_first_point && data_application.final_second_point && data_application.final_third_point){
+        if (data_application.final_first_point && data_application.final_second_point && data_application.final_third_point)
+        {
             nlohmann::json trajectory_specification;
-	        trajectory_specification["timestamp"] = return_current_time_and_date();
+            trajectory_specification["timestamp"] = return_current_time_and_date();
             {
-        	    std::stringstream target;
-	            target << (*data_application.final_first_point).format(CleanFmt) << std::endl;
-	            trajectory_specification["target"] = target.str();
+                std::stringstream target;
+                target << (*data_application.final_first_point).format(CleanFmt) << std::endl;
+                trajectory_specification["target"] = target.str();
             }
             {
-        	    std::stringstream entry;
-	            entry << (*data_application.final_third_point).format(CleanFmt) << std::endl;
-	            trajectory_specification["entry"] = entry.str();
+                std::stringstream entry;
+                entry << (*data_application.final_third_point).format(CleanFmt) << std::endl;
+                trajectory_specification["entry"] = entry.str();
             }
 
-    	    // write prettified JSON to another file
-	        std::ofstream o(CURAN_COPIED_RESOURCE_PATH"/trajectory_specification.json");
-	        o << trajectory_specification;
-	        std::cout << trajectory_specification << std::endl;
-        } else {
+            // write prettified JSON to another file
+            std::ofstream o(CURAN_COPIED_RESOURCE_PATH "/trajectory_specification.json");
+            o << trajectory_specification;
+            std::cout << trajectory_specification << std::endl;
+        }
+        else
+        {
             std::cout << "the points required to specify the trajectory \nwere not specified, please specify them" << std::endl;
+            return 1;
+        }
+
+        if (data_application.map[ORIGINAL_VOLUME].get_volume().IsNull())
+        {
+            std::cout << "the original volume is null, thus no area is contained in it" << std::endl;
+            return 1;
+        }
+
+        auto geometries = data_application.map[ORIGINAL_VOLUME].geometries();
+
+        ImageType::Pointer masked_output_image = ImageType::New();
+        DeepCopy<ImageType>(data_application.map[ORIGINAL_VOLUME].get_volume(), masked_output_image);
+
+        // we are going to print the itk image whole and the masked image
+        std::vector<std::array<double, 6>> internals;
+        internals.reserve(geometries.size());
+        for (const auto &geom : geometries)
+        {
+            std::array<double, 6> boundaries;
+            boundaries[0] = std::numeric_limits<double>::max();  // min x - 0
+            boundaries[1] = std::numeric_limits<double>::max();  // min y - 1
+            boundaries[2] = std::numeric_limits<double>::max();  // min z - 2
+            boundaries[3] = -std::numeric_limits<double>::max(); // max x - 3 
+            boundaries[4] = -std::numeric_limits<double>::max(); // max y - 4
+            boundaries[5] = -std::numeric_limits<double>::max(); // max z - 5 
+            for (const auto &vert : geom.geometry.vertices)
+            {
+                boundaries[0] = std::min((double)vert[0], boundaries[0]);
+                boundaries[1] = std::min((double)vert[1], boundaries[1]);
+                boundaries[2] = std::min((double)vert[2], boundaries[2]);
+                boundaries[0] = std::max((double)vert[0], boundaries[3]);
+                boundaries[1] = std::max((double)vert[1], boundaries[4]);
+                boundaries[2] = std::max((double)vert[2], boundaries[5]);
+            }
+            internals.push_back(boundaries);
+        }
+
+        auto evaluate_if_pixel_inside_mask = [&](double in_x, double in_y, double in_z){
+            for(const auto& boundary : internals){
+                if(in_x > boundary[0] && in_x < boundary[3] && in_y > boundary[1] && in_y < boundary[4] && in_z > boundary[2] && in_z < boundary[5])
+                    return true;
+            }
+            return false;
+        };
+
+        using IteratorType = itk::ImageRegionIteratorWithIndex<ImageType>;
+        IteratorType outputIt( masked_output_image, masked_output_image->GetRequestedRegion() );
+
+        for (outputIt.GoToBegin(); !outputIt.IsAtEnd(); ++outputIt){
+            ImageType::IndexType idx = outputIt.GetIndex();
+            if(!evaluate_if_pixel_inside_mask(idx[0]/masked_output_image->GetRequestedRegion().GetSize()[0],
+                                              idx[1]/masked_output_image->GetRequestedRegion().GetSize()[1],
+                                              idx[2]/masked_output_image->GetRequestedRegion().GetSize()[2]))
+                outputIt.Set(0);
         }
 
         return 0;
