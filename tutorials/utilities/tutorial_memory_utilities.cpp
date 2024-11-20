1cb5338e02015a9ec2379676c8516e0e187bf9b5
João Oliveira <44142506+Joaopmoliveira@users.noreply.github.com>
2024-11-20 16:34:54 +0000

    Update tutorial_memory_utilities.cpp

--- tutorials/utilities/tutorial_memory_utilities.cpp
+++ tutorials/utilities/tutorial_memory_utilities.cpp
@@ -20,9 +20,15 @@ We include <random> to be able to generate random numbers from our application
 /*
 Lastly we include the safequeue from curan
 */
-#include "utils/SafeQueue.h"
+#include "utils/MemoryUtils.h"
 
+/*
+Its important to note that the memory related manipulation of data is not as efficient as a dedicated program designed from scratch for a particular 
+purpose. If you require absolute control over memory allocation, you need to avoid these classes. 
 
+The main purpose of the memory utilities
+
+*/
 
 int main(){
 
