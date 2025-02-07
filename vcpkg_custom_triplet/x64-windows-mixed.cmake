fc40f3e0f5d0779060a00e79d4195eb6aacedb0b
João Oliveira <44142506+Joaopmoliveira@users.noreply.github.com>
2025-02-07 15:38:34 +0000

    This corrects the build system so that we can infer directly in C++ through pytorch
    The main idea is that pytorch (for some obscure reason) cannot support static linkage, thus we need to add this as dynamic linkage, but if the whole project is linked dynamically, then imgui fails because it cannot be compiled with dynamic linkage. The solution is to specify by hand which libraries are compiled statically and which libraries are compiled dynamically so that the build does not fail. This is achieved through a custom triplet

---
+++ vcpkg_custom_triplet/x64-windows-mixed.cmake
@@ -0,0 +1,23 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+
+if(${PORT} MATCHES "expat|libjpeg-turbo|libpng|libwebp|opengl|zlib|vulkan-headers|vulkan-memory-allocator|glslang|vulkan|xcb|imgui|vulkan-binding|implot|vsg|vsgimgui|skia|pugixml|assimp")
+	set(VCPKG_CRT_LINKAGE dynamic)
+	set(VCPKG_LIBRARY_LINKAGE static )
+else()
+	set(VCPKG_CRT_LINKAGE dynamic)
+	set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
